a09fe85240a3f1b1519846830d49e2fa0ddef9d8
Mihai Glonț <mglont@ebi.ac.uk>
2015-09-07 08:47:53 +0100

    Removed unused import

--- scripts/BatchImportBiomodels.groovy
+++ scripts/BatchImportBiomodels.groovy
@@ -36,7 +36,6 @@ import org.springframework.orm.hibernate3.SessionHolder
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.transaction.support.TransactionSynchronizationManager
-import org.codehaus.groovy.grails.plugins.springsecurity.SpringSecurityUtils
 import net.biomodels.jummp.annotationstore.ResourceReference
 import grails.util.Holders
 import groovy.sql.Sql
