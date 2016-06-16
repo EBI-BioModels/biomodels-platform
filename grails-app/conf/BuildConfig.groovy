eda243a0d9b8b94aca022a096437b9666d45183b
Tung Nguyen <nvntung@gmail.com>
2016-06-16 23:41:05 +0000

    update the newer version of runtime tomcat plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -142,7 +142,7 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-        build ":tomcat:7.0.54"
+        build ":tomcat:7.0.55.3"
 
         // plugins for the compile step
         compile ":cache:1.1.8"
