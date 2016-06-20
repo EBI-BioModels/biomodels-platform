f4b2ff3116ad8bf1ce7931ce0df0a57d559a7227
Tung Nguyen <nvntung@gmail.com>
2016-06-20 23:34:45 +0000

    regulate spring security settings for use of weceem

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -146,64 +146,30 @@ class BootStrap {
         }
 
         // custom mapping for weceem as it fails to work with an LDAPUserDetailsImpl
-        wcmSecurityService.securityDelegate = [
-            getUserName : { ->
-                if (springSecurityService.isLoggedIn()) {
-                    return springSecurityService.principal.username
-                } else {
-                    return null
-                }
-            },
-            getUserEmail : { ->
-                return null
-            },
-            getUserRoles : { ->
-                if (springSecurityService.isLoggedIn()) {
-                    return springSecurityService.principal.authorities
-                } else {
-                    return ['ROLE_GUEST']
-                }
-            },
-            getUserPrincipal : { ->
-                def principal = springSecurityService.getPrincipal()
-                if (principal instanceof org.springframework.security.core.userdetails.UserDetails) {
-                    return new org.springframework.security.core.userdetails.UserDetails() {
-                        Collection<org.springframework.security.core.GrantedAuthority> getAuthorities() {
-                            return principal.authorities
-                        }
-                        String getPassword() {
-                            return principal.password
-                        }
-                        String getUsername() {
-                            return principal.username
-                        }
-                        boolean isAccountNonExpired() {
-                            return principal.isAccountNonExpired()
-                        }
-                        boolean isAccountNonLocked() {
-                            return principal.isAccountNonLocked()
-                        }
-                        boolean isCredentialsNonExpired() {
-                            return principal.isCredentialsNonExpired()
-                        }
-                        boolean isEnabled() {
-                            return principal.isEnabled()
-                        }
-                        String getEmail() {
-                            return null
-                        }
-                        String getFirstName() {
-                            return null
-                        }
-                        String getLastName() {
-                            return null
-                        }
-                    }
-                } else {
-                    return principal
-                }
-            }
-        ]
+//        wcmSecurityService.securityDelegate = [
+//            getUserName : { ->
+//                def principal = springSecurityService.getPrincipal()
+//                if (principal instanceof String) {
+//                    return null
+//                } else {
+//                    return springSecurityService.getPrincipal()?.username
+//                }
+//            },
+//            getUserEmail : { ->
+//                def principal = springSecurityService.getPrincipal()
+//                if (principal instanceof String) {
+//                    return null
+//                } else {
+//                    return springSecurityService.getPrincipal()?.email
+//                }
+//            },
+//            getUserRoles : { ->
+//                springSecurityService.authentication.authorities ?: ['ROLE_ANONYMOUS']
+//            },
+//            getUserPrincipal : { ->
+//                springSecurityService.principal
+//	        }
+//        ]
         AbstractAppendingDecorator.context = ctx
         RevisionTransportCommand.context = ctx
     }
