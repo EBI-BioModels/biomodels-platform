50a1dc279cdd6cdbdc9fa66392adf5b112e1a3d3
Tung Nguyen <nvntung@gmail.com>
2016-06-24 16:03:17 +0000

    use AnnotationStore v.0.3 snapshot: upgrading groovy, grails and associated dependencies.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -83,7 +83,7 @@ grails.project.dependency.resolution = {
         runtime "postgresql:postgresql:9.1-901.jdbc4"
 
         //using the latest groovy eclipse compiler 2.9.2-01
-        compile "net.biomodels.jummp:AnnotationStore:0.2.8-SNAPSHOT"
+        compile "net.biomodels.jummp:AnnotationStore:0.3-SNAPSHOT"
         compile "org.apache.solr:solr-solrj:5.3.1"
         //required by both JSBML and SolrJ
         compile "org.codehaus.woodstox:woodstox-core-lgpl:4.4.1"
