3807dfe1ae105c6754aa25a597e6d2f7dc0dbba1
Tung Nguyen <nvntung@gmail.com>
2016-09-14 17:24:53 +0100

    try to use MarkupBuilder class for processing XML instead of StringBuilder

--- jummp-plugins/jummp-plugin-omicsdi/grails-app/services/net/biomodels/jummp/plugins/omicsdi/OmicsdiService.groovy
+++ jummp-plugins/jummp-plugin-omicsdi/grails-app/services/net/biomodels/jummp/plugins/omicsdi/OmicsdiService.groovy
@@ -5,6 +5,7 @@ import grails.util.Metadata
 import groovy.xml.MarkupBuilder
 import groovy.xml.XmlUtil
 import net.biomodels.jummp.model.Model
+import net.biomodels.jummp.model.ModelFormat
 import net.biomodels.jummp.model.Publication
 import net.biomodels.jummp.model.Revision
 import net.biomodels.jummp.plugins.security.Person
@@ -79,9 +80,22 @@ class OmicsdiService {
             e.technologyType = curationComment
 
             e.modelFormat = r.format?.name ?: "Unknown"
-            e.publicationId = publication?.id ?: ""
+            if (publication?.id) {
+                e.publicationId = publication.id
+            }
             e.submissionId = m.submissionId
-            e.validationStatus = r.validationReport
+            ModelFormat modelFormat = ModelFormat.findById(r.formatId)
+            e.levelVersion = modelFormat?.formatVersion ?: ""
+
+            e.validationStatus = r.validationReport ?: ""
+            e.certificationComment = curationComment
+
+            e.deleted = m.deleted
+            e.isPublic = m.firstPublished != null
+            e.deleted = m.deleted
+            e.certified = qcInfo  != null
+
+
             entries.add(e)
         }
         return entries
@@ -139,7 +153,7 @@ BioModels Database is a repository of computational models of biological process
             content.append("<field name=\"validationStatus\">${e.validationStatus}</field>")
             content.append("<field name=\"certificationComment\">${e.certificationComment}</field>")
             content.append("<field name=\"elementName\">${e.elementName}</field>")
-            content.append("<field name=\"elementID\">${e.elementId}</field>")
+            content.append("<field name=\"elementId\">${e.elementId}</field>")
             content.append("<field name=\"elementDescription\">${e.elementDescription}</field>")
             content.append("<field name=\"public\">${e.isPublic}</field>")
             content.append("<field name=\"deleted\">${e.deleted}</field>")
@@ -171,23 +185,166 @@ BioModels Database is a repository of computational models of biological process
         return content.toString()
     }
     void saveAsOmicsdiSchemaXML() {
-        /*String folder = "/automount/vnas-homes_vol-vol_homes-homes/tnguyen/Documents/Synchronisation/"
+        String folder = "/automount/vnas-homes_vol-vol_homes-homes/tnguyen/Documents/Synchronisation/"
         String fileName = "OmicsDISchematest.xml"
         def fileWriter = new FileWriter("${folder}${fileName}")
-        def peopleBuilder = new MarkupBuilder(fileWriter)
-        peopleBuilder.people {
-            person {
-                firstName('John')
-                lastName('Doe')
-                age(25)
-            }
-            person {
-                firstName('Jane')
-                lastName('Smith')
-                age(31)
+        def markupBuilder = new MarkupBuilder(fileWriter)
+        List<OmicsdiDataSetEntry> modelEntries = generateOmicsdiDataSetEntry()
+        String _name = 'BioModels Database'
+        String _description  = """
+BioModels Database is a repository of computational models of biological processes. Models described
+        from literature are manually curated and enriched with cross-references.
+"""
+        byte _releaseVersion = 1
+        Date _releaseDate = new Date()
+        int _entryCount = modelEntries.size() ?: 0
+//        OmicsdiDataSet bmDb = new OmicsdiDataSet(name: name, description: description,
+//            releaseVersion: 1, dateReleased: new Date(), entryCount: entryCount)
+        markupBuilder.database {
+            // add the principal information of database
+            name(_name)
+            description(_description)
+            release(_releaseVersion)
+            release_date(_releaseDate)
+            entry_count(_entryCount)
+            // add the models
+            entries {
+                modelEntries.each { e ->
+                    entry(id: "$e.id") {
+                        name("$e.name")
+                        if (e.description) {
+                            description("$e.description")
+                        }
+                        dates() {
+                            if (e.dateSubmitted) {
+                                date(type: "submission", value: "$e.dateSubmitted")
+                            }
+                            if (e.datePublished) {
+                                date(type: "publication", value: "$e.datePublished")
+                            }
+                            if (e.dateLastModified) {
+                                date(type: "last_modification", value: "$e.dateLastModified")
+                            }
+                        }
+                        additional_fields() {
+                            if (e.submitterName) {
+                                field(name: "submitter", "$e.submitterName")
+                            }
+                            if (e.submitterMail) {
+                                field(name: "submitter_mail", "$e.submitterMail")
+                            }
+                            if (e.submitterAffiliation) {
+                                field(name: "submitter_affiliation", "$e.submitterAffiliation")
+                            }
+                            if (e.repositoryName) {
+                                field(name: "repository", "$e.repositoryName")
+                            }
+                            if (e.fullDataSetLink) {
+                                field(name: "full_dataset_link", "$e.fullDataSetLink")
+                            }
+                            if (e.publication) {
+                                field(name: "publication", "$e.publication")
+                            }
+                            if (e.diseaseName) {
+                                field(name: "disease", "$e.diseaseName")
+                            }
+                            if (e.omicsType) {
+                                field(name: "omics_type", "$e.omicsType")
+                            }
+                            if (e.dataProtocol) {
+                                field(name: "data_protocol", "$e.dataProtocol")
+                            }
+                            if (e.sampleProtocol) {
+                                field(name: "sample_protocol", "$e.sampleProtocol")
+                            }
+                            if (e.technologyType) {
+                                field(name: "technology_type", "$e.technologyType")
+                            }
+                            if (e.modelFormat) {
+                                field(name: "modelFormat", "$e.modelFormat")
+                            }
+                            if (e.submissionId) {
+                                field(name: "submissionId", "$e.submissionId")
+                            }
+                            if (e.publicationId) {
+                                field(name: "publicationId", "$e.publicationId")
+                            }
+                            if (e.levelVersion) {
+                                field(name: "levelVersion", "$e.levelVersion")
+                            }
+                            if (e.validationStatus) {
+                                field(name: "validationStatus", "$e.validationStatus")
+                            }
+                            if (e.certificationComment) {
+                                field(name: "certificationComment", "$e.certificationComment")
+                            }
+                            if (e.elementName) {
+                                field(name: "elementName", "$e.elementName")
+                            }
+                            if (e.elementId) {
+                                field(name: "elementId", "$e.elementId")
+                            }
+                            if (e.elementDescription) {
+                                field(name: "elementDescription", "$e.elementDescription")
+                            }
+                            field(name: "public", "$e.isPublic")
+                            field(name: "deleted", "$e.deleted")
+                            field(name: "certified", "$e.certified")
+                            if (e.sbmlSBOTerm) {
+                                field(name: "sbmlSBOTerm", "$e.sbmlSBOTerm")
+                            }
+                            if (e.curators) {
+                                field(name: "curators", "$e.curators")
+                            }
+                            if (e.authors) {
+                                field(name: "authors", "$e.authors")
+                            }
+                            if (e.derivations) {
+                                field(name: "derivations", "$e.derivations")
+                            }
+                            if (e.pharmmlTherapeuticArea) {
+                                field(name: "pharmmlTherapeuticArea", "$e.pharmmlTherapeuticArea")
+                            }
+                            if (e.pharmmlModellingContextDescription) {
+                                field(name: "pharmmlModellingContextDescription", "$e.pharmmlModellingContextDescription")
+                            }
+                            if (e.pharmmlLongTechnicalDescription) {
+                                field(name: "pharmmlLongTechnicalDescription", "$e.pharmmlLongTechnicalDescription")
+                            }
+                            if (e.pharmmlShortDescription) {
+                                field(name: "pharmmlShortDescription", "$e.pharmmlShortDescription")
+                            }
+                            if (e.pharmmlPublicationSource) {
+                                field(name: "pharmmlPublicationSource", "$e.pharmmlPublicationSource")
+                            }
+                            if (e.pharmmlImplementationConformsToLiterature) {
+                                field(name: "pharmmlImplementationConformsToLiterature", "$e.pharmmlImplementationConformsToLiterature")
+                            }
+                            if (e.pharmmlImplementationDiscrepancies) {
+                                field(name: "pharmmlImplementationDiscrepancies", "$e.pharmmlImplementationDiscrepancies")
+                            }
+                            if (e.pharmmlModelDevelopmentContext) {
+                                field(name: "pharmmlModelDevelopmentContext", "$e.pharmmlModelDevelopmentContext")
+                            }
+                            if (e.pharmmlCodeFromLiterature) {
+                                field(name: "pharmmlCodeFromLiterature", "$e.pharmmlCodeFromLiterature")
+                            }
+                            if (e.pharmmlResearchStage) {
+                                field(name: "pharmmlResearchStage", "$e.pharmmlResearchStage")
+                            }
+                            if (e.pharmmlTasks) {
+                                field(name: "pharmmlTasks", "$e.pharmmlTasks")
+                            }
+                            if (e.pharmmlTypeOfData) {
+                                field(name: "pharmmlTypeOfData", "$e.pharmmlTypeOfData")
+                            }
+                        }
+                    }
+                }
             }
         }
-        fileWriter.close()*/
+        fileWriter.close()
+
         def writer = new FileWriter('/homes/tnguyen/tmp/file.xml')
         def text = buildOmicsdiSchemaXml()
         def xml = new XmlSlurper().parseText(text)
