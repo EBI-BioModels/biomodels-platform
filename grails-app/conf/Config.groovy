cb8c2c8d54a2df1ac9d488b67910ee966ebd35b0
Tung Nguyen <nvntung@gmail.com>
2016-10-06 16:00:06 +0100

    use values configured in jummp properties file

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -714,11 +714,20 @@ jummp.config.maintenance = false
 
 jummp.id.generators = ModelIdentifierUtils.processGeneratorSettings(jummp)
 
-jummp.metadata.officialDatabaseName = 'New BioModels Database'
-jummp.metadata.officialDatabaseDescription = """
-BioModels Database is a repository of computational models of biological processes. Models described
-        from literature are manually curated and enriched with cross-references.
-"""
+if (!(jummpConfig.jummp.metadata.officialDatabaseName instanceof ConfigObject)) {
+    jummp.metadata.officialDatabaseName = jummpConfig.jummp.metadata.officialDatabaseName
+} else {
+    jummp.metadata.officialDatabaseName = 'BioModels Database'
+}
+
+if (!(jummpConfig.jummp.metadata.officialDatabaseDescription instanceof ConfigObject)) {
+    jummp.metadata.officialDatabaseDescription = jummpConfig.jummp.metadata.officialDatabaseDescription
+} else {
+    jummp.metadata.officialDatabaseDescription = """\
+        BioModels Database is a repository of computational models of biological processes.
+        Models described from literature are manually curated and enriched with cross-references.
+        """
+}
 // elasticsearch settings for weceem
 elasticSearch.datastoreImpl = 'hibernateDatastore'
 elasticSearch.bulkIndexOnStartup = true
