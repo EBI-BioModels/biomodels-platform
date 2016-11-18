63da650251c8f8c0ac981dc7a4a5296fe4f6d5ed
Mihai Glonț <mglont@ebi.ac.uk>
2016-11-18 17:58:31 +0000

    added missing dependencies for core api plugin.

--- jummp-plugins/jummp-plugin-core-api/grails-app/conf/BuildConfig.groovy
+++ jummp-plugins/jummp-plugin-core-api/grails-app/conf/BuildConfig.groovy
@@ -71,6 +71,16 @@ grails.project.dependency.resolution = {
     }
     dependencies {
         compile("eu.ddmore.pharmml:libPharmML:0.4-beta-b3")
+        compile("net.biomodels.jummp:AnnotationStore:0.3.1-SNAPSHOT")
+        compile ("eu.ddmore.metadata:lib-metadata:1.5.2-SNAPSHOT") {
+            excludes 'spring-context','spring-core','spring-test', 'jena'
+        }
+        compile "uk.ac.ebi.ddi:ddi-ebe-ws-dao:1.0-SNAPSHOT"
+        // Jackson DataBinder has 'provided' scope in DDI: See
+        //      https://github.com/BD2K-DDI/ddi-base-master/blob/2326b4/pom.xml
+        //      https://github.com/BD2K-DDI/ddi-ebeye-ws-dao/blob/8bd08f/pom.xml
+        compile "com.fasterxml.jackson.core:jackson-databind:2.5.2"
+        compile "org.apache.commons:commons-lang3:3.3.2"
     }
     plugins {
         build ":tomcat:7.0.55.3"
