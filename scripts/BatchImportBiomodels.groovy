01b19c003c930a47c56e4d3e1a0646dea354a7c8
Mihai Glonț <mglont@ebi.ac.uk>
2017-01-27 14:00:59 +0000

    JBM-19 added element type when importer saves custom annotations

--- scripts/BatchImportBiomodels.groovy
+++ scripts/BatchImportBiomodels.groovy
@@ -189,6 +189,7 @@ def ResourceReference
 def Qualifier
 def Statement
 def ElementAnnotation
+def ModelElementType
 def PublicationLinkProvider
 def LinkType
 
@@ -524,7 +525,7 @@ target(loadClasses: 'Loads required classes in the Jummp Grails environment') {
     Statement = loadClass("net.biomodels.jummp.annotationstore.Statement")
     ElementAnnotation = loadClass("net.biomodels.jummp.annotationstore.ElementAnnotation")
     Qualifier = loadClass("net.biomodels.jummp.annotationstore.Qualifier")
-
+    ModelElementType = loadClass("net.biomodels.jummp.model.ModelElementType")
     // BioModels-specific domain classes
     CurationNotes = loadClass("net.biomodels.jummp.deployment.biomodels.CurationNotes")
     ModelOfTheMonth = loadClass("net.biomodels.jummp.deployment.biomodels.ModelOfTheMonth")
@@ -1515,11 +1516,10 @@ createBMAnnotation = { revision, object, qual, creator ->
         qualifier.save(failOnError:true)
     }
     def statement = Statement.newInstance(subjectId: 'modelLevelAnnotation',
-                                          qualifier: qualifier,
-                                          object: resourceRef)
+            qualifier: qualifier, object: resourceRef)
+    def modelElementType = ModelElementType.findByModelFormatAndName(revision.format, 'model')
     def elementAnnotation = ElementAnnotation.newInstance(creatorId: creator,
-                                                          statement: statement,
-                                                          revision: revision)
+            statement: statement, revision: revision, modelElementType: modelElementType)
     elementAnnotation.save(failOnError:true)
 }
 
