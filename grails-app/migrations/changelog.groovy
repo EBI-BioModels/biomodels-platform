e7fcba49e38b99864aa99af43a47238385d9198c
Mihai Glonț <mglont@ebi.ac.uk>
2017-02-06 14:47:15 +0000

    added db migration for revision annotations association.

--- grails-app/migrations/changelog.groovy
+++ grails-app/migrations/changelog.groovy
@@ -1225,5 +1225,7 @@ databaseChangeLog = {
     include file: '20161212_publicationPropertiesAsString.groovy'
 
 	include file: '2017-02-01_make_publication_details_nullable.groovy'
+
+	include file: '20170206_fixRevisionAnnotationAssociations.groovy'
 }
 
