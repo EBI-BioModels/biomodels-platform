29235cec608f0b17414cacc67882aef89295c6cc
Tung Nguyen <nvntung@gmail.com>
2017-06-06 15:06:56 +0100

    improved visual separation between the entries in the access statistics widgets on the front page (see the ticket JBM-110 and modifications in weceem)

--- jummp-plugins/jummp-plugin-biomodels-dom/grails-app/taglib/net/biomodels/jummp/deployment/biomodels/BioModelsTagLib.groovy
+++ jummp-plugins/jummp-plugin-biomodels-dom/grails-app/taglib/net/biomodels/jummp/deployment/biomodels/BioModelsTagLib.groovy
@@ -104,29 +104,37 @@ class BioModelsTagLib {
 
     def renderRecentlyAccessedModels = {
         Map<ModelTransportCommand, Integer> models = decorationService.getRecentlyAccessedModels()
-        StringBuilder result = new StringBuilder()
+        StringBuilder result = new StringBuilder("<ul style='list-style: none; " +
+            "list-style-position: inside; padding: 0; margin-left: 0'>")
         models?.each {
             ModelTransportCommand mtc = it.key
 	        String modelId = mtc.publicationId ?: mtc.submissionId
             String modelURI = g.createLink(controller: 'model', id: modelId, action: 'show')
             RevisionTransportCommand rtc = modelDelegateService.getLatestRevision(modelId)
-	        String modelLink = "<a href='${modelURI}'>${rtc.name}</a><br/>"
+	        String modelLink = "<li style='text-indent: -1.2em; padding-left: 1em'>" +
+                "<span class='icon icon-functional' data-icon='4'>&nbsp;</span>" +
+                "<a href='${modelURI}'>${rtc.name}</a></li>"
             result.append(modelLink)
         }
+	    result.append("</ul>")
         out << result.toString()
     }
 
     def renderRecentlyPublishedModels = {
         Map<ModelTransportCommand, Date> models = decorationService.getRecentlyPublishedModels()
-        StringBuilder result = new StringBuilder()
+        StringBuilder result = new StringBuilder("<ul style='list-style: none; " +
+            "list-style-position: inside; padding: 0; margin-left: 0'>")
         models?.each {
             ModelTransportCommand mtc = it.key
 	        String modelId = mtc.publicationId ?: mtc.submissionId
             RevisionTransportCommand rtc = modelDelegateService.getLatestRevision(modelId)
             String modelURI = g.createLink(controller: 'model', id: modelId, action: 'show')
-            String modelLink= "<a href='${modelURI}'>${rtc.name}</a><br/>"
+            String modelLink= "<li style='text-indent: -1.2em; padding-left: 1em'>" +
+                "<span class='icon icon-functional' data-icon='U'>&nbsp;</span>" +
+                "<a href='${modelURI}'>${rtc.name}</a></li>"
             result.append(modelLink)
         }
+	    result.append("</ul>")
         out << result.toString()
     }
 }
