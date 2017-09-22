6266416b1de47be014649287d833ecd38a98c3fd
Tung Nguyen <nvntung@gmail.com>
2017-09-22 15:53:20 +0100

    make few of minor changes

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -91,7 +91,6 @@ grails.converters.encoding = "UTF-8"
 grails.views.gsp.sitemesh.preprocess = true
 // scaffolding templates configuration
 grails.scaffolding.templates.domainSuffix = 'Instance'
-
 // Set to false to use the new Grails 1.2 JSONBuilder in the render method
 grails.json.legacy.builder = false
 // enabled native2ascii conversion of i18n properties files
@@ -100,22 +99,17 @@ grails.enable.native2ascii = true
 grails.logging.jul.usebridge = true
 // packages to include in Spring bean scanning
 grails.spring.bean.packages = []
-
 // The default scope for controllers. May be prototype, session or singleton.
 // If unspecified, controllers are prototype scoped.
 grails.controllers.defaultScope = 'singleton'
-
 // configure auto-caching of queries by default (if false you can cache individual queries with 'cache: true')
 grails.hibernate.cache.queries = false
-
 // configure passing transaction's read-only attribute to Hibernate session, queries and criterias
 // set "singleSession = false" OSIV mode in hibernate configuration after enabling
 grails.hibernate.pass.readonly = false
 // configure passing read-only to OSIV session by default, requires "singleSession = false" OSIV mode
 grails.hibernate.osiv.readonly = false
-
 grails.views.javascript.library="jquery"
-
 // avoid ehcache duplicate CacheManager exception mess
 beans {
     cacheManager {
@@ -145,7 +139,7 @@ environments {
    development {
       grails.logging.jul.usebridge = false
       grails.plugin.springsecurity.debug.useFilter = false
-      // enable/disable console plugin
+      // enable/disable web-based console plugin
       grails.plugin.console.enabled = true
    }
    production {
--- jummp-plugins/jummp-plugin-web-application/grails-app/controllers/net/biomodels/jummp/webapp/NotificationController.groovy
+++ jummp-plugins/jummp-plugin-web-application/grails-app/controllers/net/biomodels/jummp/webapp/NotificationController.groovy
@@ -32,21 +32,21 @@ class NotificationController {
 
     @Secured(["isAuthenticated()"])
 	def unreadNotificationCount() {
-		render notificationService.unreadNotificationCount();
+		render notificationService.unreadNotificationCount()
 	}
 
 	@Secured(["isAuthenticated()"])
 	def list() {
 		if (params.all) {
-			return [notifications: notificationService.list(userService.getCurrentUser().username), partial: false];
+			return [notifications: notificationService.list(userService.getCurrentUser().username), partial: false]
 		}
 		else {
-			return [notifications: notificationService.list(userService.getCurrentUser().username, 10), partial: true];
+			return [notifications: notificationService.list(userService.getCurrentUser().username, 10), partial: true]
 		}
 	}
 
 	@Secured(["isAuthenticated()"])
 	def markAsRead(int msg) {
-		notificationService.markAsRead(msg, userService.getCurrentUser().username);
+		notificationService.markAsRead(msg, userService.getCurrentUser().username)
 	}
 }
