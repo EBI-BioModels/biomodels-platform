73e6557196a11c950e1c1d6a0ec70b7a624de792
Tung Nguyen <nvntung@gmail.com>
2017-11-03 23:38:15 +0000

    JBM-179 implement methods of updating CurationNotes at business logical layer and controller tier

--- jummp-plugins/jummp-plugin-biomodels-dom/grails-app/controllers/net/biomodels/jummp/deployment/biomodels/CurationNotesController.groovy
+++ jummp-plugins/jummp-plugin-biomodels-dom/grails-app/controllers/net/biomodels/jummp/deployment/biomodels/CurationNotesController.groovy
@@ -1,6 +1,13 @@
 package net.biomodels.jummp.deployment.biomodels
 
 import grails.plugin.springsecurity.annotation.Secured
+import groovy.json.JsonSlurper
+import net.biomodels.jummp.core.adapters.ModelAdapter
+import net.biomodels.jummp.core.model.ModelTransportCommand
+import net.biomodels.jummp.model.Model
+import net.biomodels.jummp.plugins.security.User
+
+import java.text.SimpleDateFormat
 
 /**
  * @short Service responsible for retrieving CurationNotes entries.
@@ -11,6 +18,7 @@ import grails.plugin.springsecurity.annotation.Secured
  */
 @Secured(['ROLE_CURATOR'])
 class CurationNotesController {
+    def curationNotesService
 
     def index() {
 
@@ -20,7 +28,44 @@ class CurationNotesController {
         render(view: "edit")
     }
 
-    private update() {
+    def updateCurationImage() {
+        curationNotesService.updateCurationImage(params.model, params.curaImg)
+        render "Curation image has been updated successfully"
+    }
+
+    def update() {
+        def curationNotes = new JsonSlurper().parseText(params.curationNotes)
+        Long id = curationNotes["id"]
+        String modelId = params.model
+        String comment = curationNotes["comment"]
+        String submitterUsername = curationNotes["submitter"]
+        User submitter = User.findByUsername(submitterUsername)
+        String lastModifierUsername = curationNotes["lastModifier"]
+        User lastModifier = User.findByUsername(lastModifierUsername)
+        def newDateAdded = curationNotes["dateAdded"]
+        def newLastModified = curationNotes["lastModified"]
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+        Date dateAdded = dateFormat.parse(newDateAdded)
+        Date lastModified = dateFormat.parse(newLastModified)
+        Model model = Model.findByPublicationIdOrSubmissionId(modelId, modelId)
+        ModelTransportCommand modeltc = new ModelAdapter(model: model).toCommandObject()
+        def bindingMap = [id: id,
+                          model: modeltc,
+                          comment: comment,
+                          submitter: submitter,
+                          lastModifier: lastModifier,
+                          dateAdded: dateAdded,
+                          lastModified: lastModified]
+        CurationNotesTransportCommand command = new CurationNotesTransportCommand(bindingMap)
+        boolean status = curationNotesService.updateCurationNotes(command)
+        if (status) {
+            render "Simulation results have been updated successfully"
+        } else {
+            render "There is an error while it tries to save the curation notes"
+        }
+    }
+
+    def reset() {
 
     }
 }
--- jummp-plugins/jummp-plugin-biomodels-dom/grails-app/services/net/biomodels/jummp/deployment/biomodels/CurationNotesService.groovy
+++ jummp-plugins/jummp-plugin-biomodels-dom/grails-app/services/net/biomodels/jummp/deployment/biomodels/CurationNotesService.groovy
@@ -25,16 +25,23 @@
 package net.biomodels.jummp.deployment.biomodels
 
 import grails.transaction.Transactional
+import net.biomodels.jummp.model.Model
+import org.apache.commons.logging.Log
+import org.apache.commons.logging.LogFactory
 
 /**
  * @short Service responsible for retrieving CurationNotes entries.
- *
  * This class  is used for dealing with CurationNotes records.
  *
  * @author Tung Nguyen <tung.nguyen@ebi.ac.uk>
  */
 @Transactional
 class CurationNotesService {
+    static final Log log = LogFactory.getLog(this.getClass())
+    /**
+     * Flag indicating the logger's verbosity threshold.
+     */
+    static final boolean IS_DEBUG_ENABLED = log.isDebugEnabled()
     /**
      * Retrieve the latest modified curation notes based on given model id
      *
@@ -66,5 +73,49 @@ class CurationNotesService {
             }
         }
         return latestCurationNotes
+    }
+
+    /**
+     * Update curation image as a part of the simulation results
+     * @param modelSubmissionId A string represents model identifier
+     * @param newCuraImg        A string represents the image encoded as Base64
+     */
+    void updateCurationImage(String modelSubmissionId, String newCuraImg) {
+        Model model = Model.findBySubmissionIdOrPublicationId(modelSubmissionId, modelSubmissionId)
+        Long modelId = model.id
+        byte[] newCuraImgByteArr = Base64.decoder.decode(newCuraImg)
+        updateCurationImage(modelId, newCuraImgByteArr)
+    }
+
+    void updateCurationImage(Long modelId, byte[] newCuraImg) {
+        CurationNotesTransportCommand cntc = fetchCurationNotesForModel(modelId)
+        if (cntc) {
+            Model model = Model.get(modelId)
+            CurationNotes cn = CurationNotes.findBySubmitterAndLastModifierAndModel(cntc.submitter, cntc.lastModifier, model)
+            cn.curationImage = newCuraImg
+            cn.save(flush: true)
+        }
+    }
+
+    boolean updateCurationNotes(CurationNotesTransportCommand cntc) {
+        Model model = Model.get(cntc.model.id)
+        Long id = cntc.id
+        CurationNotes cn = CurationNotes.get(id)
+        if (cn) {
+            cn.comment = cntc.comment
+            cn.submitter = cntc.submitter
+            cn.lastModifier = cntc.lastModifier
+            cn.dateAdded = cntc.dateAdded
+            cn.lastModified = cntc.lastModified
+            if (cn.save(flush: true)) {
+                if (IS_DEBUG_ENABLED) {
+                    log.debug("The simulation results of the model $model.id have been saved!")
+                }
+                return true
+            } else {
+                throw new Exception("Failed to try persisting curation notes")
+            }
+        }
+        false
     }
 }
