28703f30548d08664a723fa0b5dd257c2b880b77
Tung Nguyen <nvntung@gmail.com>
2017-11-03 18:12:59 +0000

    JBM-179 ease the retrieval of CurationNotes records via its command object

--- jummp-plugins/jummp-plugin-biomodels-dom/src/groovy/net/biomodels/jummp/deployment/biomodels/CurationNotesCategory.groovy
+++ jummp-plugins/jummp-plugin-biomodels-dom/src/groovy/net/biomodels/jummp/deployment/biomodels/CurationNotesCategory.groovy
@@ -39,6 +39,7 @@ class CurationNotesCategory {
      */
     CurationNotesTransportCommand toCommandObject() {
         new CurationNotesTransportCommand(
+            id: this.id,
             model: new ModelAdapter(model: model).toCommandObject(),
             submitter: this.submitter, lastModifier: this.lastModifier,
             dateAdded: this.dateAdded, lastModified: this.lastModified,
--- jummp-plugins/jummp-plugin-biomodels-dom/src/groovy/net/biomodels/jummp/deployment/biomodels/CurationNotesTransportCommand.groovy
+++ jummp-plugins/jummp-plugin-biomodels-dom/src/groovy/net/biomodels/jummp/deployment/biomodels/CurationNotesTransportCommand.groovy
@@ -30,6 +30,7 @@ import net.biomodels.jummp.plugins.security.User
  */
 class CurationNotesTransportCommand implements Serializable {
     private static final long serialVersionUID = 1L
+    Long id
     ModelTransportCommand model
     User submitter
     User lastModifier
