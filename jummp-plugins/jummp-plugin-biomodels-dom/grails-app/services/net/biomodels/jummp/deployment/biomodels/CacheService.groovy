7a27dafb3681aaf5bc1dac3542d83a50c4070aa0
tvu <glmanhtu@gmail.com>
2018-03-08 16:59:05 +0000

    [+] Add Cache Service

---
+++ jummp-plugins/jummp-plugin-biomodels-dom/grails-app/services/net/biomodels/jummp/deployment/biomodels/CacheService.groovy
@@ -0,0 +1,110 @@
+package net.biomodels.jummp.deployment.biomodels
+
+import grails.util.Holders
+import net.biomodels.jummp.models.Cache
+import net.biomodels.jummp.utils.TimeUtils
+
+import java.nio.file.Files
+
+class CacheService {
+
+    private Map<String, Cache<Long, Serializable>> cached = new HashMap<>()
+
+    CacheService() {
+        reloadCachedFiles(getCacheDir())
+    }
+
+    /**
+     * Get cache dir path from config then create File object from it
+     * @return
+     */
+    File getCacheDir() {
+        String cacheDirString = Holders.grailsApplication.config.jummp.cache.dir
+        return new File(cacheDirString)
+    }
+
+    /**
+     * Reload all cached files from cacheDir
+     * @param cacheDir
+     */
+    void reloadCachedFiles(File cacheDir) {
+        for (final File fileEntry : cacheDir.listFiles()) {
+            if (!fileEntry.isDirectory()) {
+                cached.put(fileEntry.getName(), loadObjectFromFile(fileEntry))
+            }
+        }
+    }
+
+    /**
+     * Check whether we have cache for the given name or not
+     * We also check if the time of cache was expired then remove it
+     *
+     * @param name
+     * @return
+     */
+    boolean hasCache(String name) {
+        if (cached.containsKey(name)) {
+            if (cached.get(name).getKey() > TimeUtils.currentTimestamp) {
+                return true;
+            }
+            removeCache(name)
+        }
+        return false
+    }
+
+    void removeCache(String name) {
+        if (cached.containsKey(name)) {
+            cached.remove(name)
+            File cache = new File(getCacheDir(), name)
+            Files.deleteIfExists(cache.toPath())
+        }
+    }
+
+    /**
+     * Get back cache object
+     * @param name
+     * @return
+     */
+    Serializable getCache(String name) {
+        return cached.get(name).getValue();
+    }
+
+    /**
+     * Set a new cache
+     * @param name key
+     * @param value
+     * @param expired number of second from now when the cache will be expire
+     */
+    void setCache(String name, Serializable value , int expired) {
+        if (hasCache(name)) {
+            cached.get(name).setValue(value);
+        } else {
+            cached.put(name, new Cache<Long, Serializable>(TimeUtils.currentTimestamp + expired, value))
+        }
+        writeObjectToFile(getCacheDir(), name, cached.get(name))
+    }
+
+    /**
+     * Write object to file for future recover
+     * @param name
+     * @param object
+     */
+    void writeObjectToFile(File cacheDir, String name, Serializable object) {
+        FileOutputStream cacheFile = new FileOutputStream(new File(cacheDir, name))
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(cacheFile)
+        objectOutputStream.writeObject(object)
+        objectOutputStream.close()
+        cacheFile.close()
+    }
+
+    /**
+     * Load object from file
+     * @param file
+     * @return
+     */
+    Cache<Long, Serializable> loadObjectFromFile(File file) {
+        return new FileInputStream(file).withObjectInputStream(getClass().classLoader) {
+            is -> is.readObject() as Cache<Long, Serializable>
+        }
+    }
+}
---
+++ jummp-plugins/jummp-plugin-biomodels-dom/test/unit/net/biomodels/jummp/deployment/biomodels/CacheServiceSpec.groovy
@@ -0,0 +1,59 @@
+package net.biomodels.jummp.deployment.biomodels
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(CacheService)
+class CacheServiceSpec extends Specification {
+
+    String cacheDir = "/nfs/production3/biomodels/work/jummp/cache/test"
+
+    def setup() {
+        grailsApplication.config.jummp.cache.dir = cacheDir
+    }
+
+    def cleanup() {
+        File testDir = new File(cacheDir)
+        for (File testFile : testDir.listFiles()) {
+            if (testFile.isFile()) {
+                testFile.delete()
+            }
+        }
+    }
+
+    void "get object not exist"() {
+        when: 'check whether it has cache for a non exist object'
+        String cacheName = 'non-exist'
+        then: 'must return false'
+        !service.hasCache(cacheName)
+    }
+
+    void "create a new cache"() {
+        when: 'create a new cache'
+        String cacheName = 'cache-exists'
+        service.setCache(cacheName, 1, 100)
+        then: 'must create a new cache file'
+        File file = new File(cacheDir, cacheName)
+        file.isFile()
+        and: 'must return the same object value'
+        1 == service.getCache(cacheName)
+    }
+
+    void "get object that exist"() {
+        when: 'create new cache and get it back'
+        String cacheName = 'cache-exists-2'
+        service.setCache(cacheName, 1, 5)
+        then: 'must return that object exists'
+        service.hasCache(cacheName)
+        when: 'time expired'
+        Thread.sleep(5000)
+        then: 'must return that object no longer exists'
+        !service.hasCache(cacheName)
+        and: 'must remove the cache file'
+        File file = new File(cacheDir, cacheName)
+        !file.isFile()
+    }
+}
