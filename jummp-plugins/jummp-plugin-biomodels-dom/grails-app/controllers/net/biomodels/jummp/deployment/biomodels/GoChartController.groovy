97259eb444c063286d2994f1648e5f1ead328b94
tvu <glmanhtu@gmail.com>
2018-03-21 15:24:34 +0000

    [*] Switched to groove .each instead of java for

--- jummp-plugins/jummp-plugin-biomodels-dom/grails-app/controllers/net/biomodels/jummp/deployment/biomodels/GoChartController.groovy
+++ jummp-plugins/jummp-plugin-biomodels-dom/grails-app/controllers/net/biomodels/jummp/deployment/biomodels/GoChartController.groovy
@@ -1,6 +1,5 @@
 package net.biomodels.jummp.deployment.biomodels
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.node.ArrayNode
 import com.fasterxml.jackson.databind.node.ObjectNode
 import grails.plugin.springsecurity.annotation.Secured
@@ -9,7 +8,6 @@ import net.biomodels.jummp.model.Model
 import net.biomodels.jummp.models.JummpEntry
 import net.biomodels.jummp.models.ModelDetails
 import net.biomodels.jummp.models.RefData
-import net.biomodels.jummp.utils.TimeUtils
 
 @Secured(['IS_AUTHENTICATED_ANONYMOUSLY'])
 class GoChartController {
@@ -30,9 +28,8 @@ class GoChartController {
     def objectMapper
 
     def index() {
-        List<List<Model, String, String, Date, String, Long, String>> data =
-            modelService.getAllModelWithDetails(0, 0, true, ModelListSorting.ID)
-        List<ModelDetails> models = data.collect{new ModelDetails(it[0], it[1], it[3])}
+        List data = modelService.getAllModelWithDetails(0, 0, true, ModelListSorting.ID)
+        List<ModelDetails> models = data.collect{new ModelDetails(it[0] as Model, it[1] as String, it[3] as Date)}
         Map<?, ?> classified = modelClassifierService.classifyModels(models)
         ArrayNode converted = convertToJson(classified, new RefData<Integer>(0))
         ['classifiedModels': converted]
@@ -40,7 +37,7 @@ class GoChartController {
 
     ArrayNode convertToJson(Map<?, ?> classified, RefData<Integer> refInt) {
         ArrayNode arrayNode = objectMapper.createArrayNode()
-        for (JummpEntry<String, String> key : (classified.keySet() as List<JummpEntry<String, String>>)) {
+        classified.each { JummpEntry<String, String> key, value ->
             RefData<Integer> total = new RefData<>(0)
             ObjectNode node = objectMapper.createObjectNode()
             if (key.value != null) {
@@ -49,14 +46,14 @@ class GoChartController {
                 node.put("name", key.key)
             }
             node.put("code", key.key)
-            if (classified.get(key) instanceof Map) {
+            if (value instanceof Map) {
                 RefData<Integer> count = new RefData<>(0)
-                node.putArray("children").addAll(convertToJson(classified.get(key) as Map<?, ?>, count))
+                node.putArray("children").addAll(convertToJson(value as Map<?, ?>, count))
                 total.setData(total.getData() + count.data)
             } else {
                 ArrayNode modelNodes = objectMapper.createArrayNode()
 
-                for (ModelDetails model : (classified.get(key) as List<ModelDetails>)) {
+                for (ModelDetails model : (value as List<ModelDetails>)) {
                     total.data += 1
                     ObjectNode child = objectMapper.createObjectNode()
                     String modelId = model.model.getPublicationId()
