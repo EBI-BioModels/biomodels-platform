7192f8867709ec41a48c5900d70c4681382a3927
tvu <glmanhtu@gmail.com>
2018-03-29 15:27:47 +0000

    Refs JBM-269 [+] Add db migration script to roll back the UNDER_CURATION state to UNPUBLISHED state

--- grails-app/migrations/20180327_add_curation_state.groovy
+++ grails-app/migrations/20180327_add_curation_state.groovy
@@ -17,4 +17,14 @@ databaseChangeLog = {
             }
         }
     }
+
+    changeSet(author: "tvu (generated)", id: "1522163825443-1") {
+        grailsChange {
+            change {
+                query = "UPDATE revision SET revision.curation_state = 'UNPUBLISHED' " +
+                    "WHERE revision.state = 'UNDER_CURATION' "
+                sql.executeUpdate(query)
+            }
+        }
+    }
 }
