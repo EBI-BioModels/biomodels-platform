ed7b7bf51344f5bbaac6eadff627adddee47eaf3
Tung Nguyen <nvntung@gmail.com>
2018-04-04 17:38:46 +0100

    JBM-274 use the release version 0.3.2 of AnnotationStore

--- jummp-plugins/jummp-plugin-core-api/grails-app/conf/BuildConfig.groovy
+++ jummp-plugins/jummp-plugin-core-api/grails-app/conf/BuildConfig.groovy
@@ -71,7 +71,7 @@ grails.project.dependency.resolution = {
     }
     dependencies {
         compile("eu.ddmore.pharmml:libPharmML:0.4-beta-b3")
-        compile("net.biomodels.jummp:AnnotationStore:0.3.2-SNAPSHOT") {
+        compile("net.biomodels.jummp:AnnotationStore:0.3.2") {
             excludes 'slf4j-log4j12'
         }
         compile("eu.ddmore.metadata:lib-metadata:1.5.2-SNAPSHOT") {
