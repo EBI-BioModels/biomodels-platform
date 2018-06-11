2ecd8e60eb8e1853372e39d2d643ecc4843753b3
tvu <glmanhtu@gmail.com>
2018-06-11 11:23:29 +0000

    JBM-293 Implement test Alway Available LSF feature
    Check if the service available during 2 days

--- test/integration/net/biomodels/jummp/core/LsfServiceTests.groovy
+++ test/integration/net/biomodels/jummp/core/LsfServiceTests.groovy
@@ -1,6 +1,8 @@
 package net.biomodels.jummp.core
 
+import net.biomodels.jummp.core.model.LSFClusterServer
 import net.biomodels.jummp.utils.NetworkUtils
+import org.junit.Assert
 
 import static org.junit.Assert.*
 import grails.test.mixin.TestMixin
@@ -17,12 +19,12 @@ class LsfServiceTests extends JummpIntegrationTest {
     def lsfService
 
     @Test
-    void testLFSService() {
+    void testLSFService() {
         String jobId = lsfService.startLFSClusterJob(LSFApplication.MODEL_CLASSIFIER, 2000, 2, 1000)
         assertNotNull(jobId)
-        String host = lsfService.getHostByJobId(jobId)
+        String host = lsfService.getHost(jobId)
         assertNotNull(host)
-        Integer port = lsfService.getPortByJobId(jobId)
+        Integer port = lsfService.getPort(jobId)
         assertNotNull(port)
 
         // Wait to make sure that our job in cluster is ready
@@ -30,5 +32,22 @@ class LsfServiceTests extends JummpIntegrationTest {
         assertTrue(NetworkUtils.isReachable(host, port))
         lsfService.stopLSFClusterJob(jobId)
         assertFalse(NetworkUtils.isReachable(host, port))
+    }
+
+    /**
+     * Test always available service
+     * This test will run during 2 days, check every 2s to see if the server reachable or not
+     */
+    @Test
+    void testAlwaysAvailableLSFService() {
+        LSFClusterServer server =
+            lsfService.startAlwaysAvailableLSFClusterJob(LSFApplication.MODEL_CLASSIFIER, 2000, 2)
+
+        Date dateEnd = new Date().plus(2)
+        while (new Date().before(dateEnd)) {
+            Assert.assertTrue(
+                "$server.hostName:$server.port", NetworkUtils.isReachable(server.hostName, server.port))
+            Thread.sleep(2000)
+        }
     }
 }
