563f3cbf52a0e3bc7386aa210f384cb4622fc189
Mihai Glont <mihai.glont@gmail.com>
2018-06-17 09:48:12 +0100

    JBM-299 allow content negotiation for Firefox&Chrome.
    This is based on the Accept request header. According to the Grails 2.5.5
    docs[1] on content negotiation, the value of this header is ignored for
    requests made using any of the four main browsers due to the fact that
    historically they were using the header Accept */*, which is not very
    helpful.
    
    Modern browsers have updated their content negotiation strategy, so it's
    okay to rely on the Accept header information they provide.
    
    [1] https://grails.github.io/grails2-doc/2.5.5/guide/single.html#contentNegotiation

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -62,6 +62,8 @@ try {
 def jummpConfig = new ConfigSlurper().parse(jummpProperties)
 List pluginsToExclude = []
 
+// The Accept header set by older versions of IE and Opera may be unreliable, ignore it
+grails.mime.disable.accept.header.userAgents = ['Presto', 'Trident']
 grails.mime.file.extensions = false // enables the parsing of file extensions from URLs into the request format
 grails.mime.use.accept.header = true
 grails.mime.types = [ html: ['text/html','application/xhtml+xml'],
