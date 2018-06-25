0a30e92a86a47e69f3e8762e1498ef9dd647c851
Tung Nguyen <tnguyen@windows.ebi.ac.uk>
2018-06-25 11:16:26 +0100

    JBM-295 improve the logic business for loading, fetching or creating publication
    This patch is introduced to
    * help to fix NPE by adding null safe operator to avoid unexpected
      NPE.
    * reorganise codes to reuse the method of getting publication
      (if it is possible) in different places.
    * shorten few imports.

--- grails-app/services/net/biomodels/jummp/core/SubmissionService.groovy
+++ grails-app/services/net/biomodels/jummp/core/SubmissionService.groovy
@@ -449,8 +449,8 @@ class SubmissionService {
         void updatePublicationLink(Map<String, Object> workingMemory, Map<String, String> modifications) {
             if (modifications.containsKey("PubLinkProvider")) {
                 MTC mtc = workingMemory.get("ModelTC") as MTC
-                workingMemory.put("previousPubLinkProvider", mtc.publication.linkProvider)
-                workingMemory.put("previousPubLink", mtc.publication.link)
+                workingMemory.put("previousPubLinkProvider", mtc.publication?.linkProvider)
+                workingMemory.put("previousPubLink", mtc.publication?.link)
                 workingMemory.put("RetrievePubDetails",
                     updatePubs(mtc,
                         modifications.get("PubLinkProvider"),
--- jummp-plugins/jummp-plugin-web-application/grails-app/controllers/net/biomodels/jummp/webapp/ModelController.groovy
+++ jummp-plugins/jummp-plugin-web-application/grails-app/controllers/net/biomodels/jummp/webapp/ModelController.groovy
@@ -40,12 +40,13 @@ import groovy.json.JsonSlurper
 import net.biomodels.jummp.core.adapters.RevisionAdapter
 import net.biomodels.jummp.core.model.*
 import net.biomodels.jummp.core.model.ModelFormatTransportCommand as MFTC
+import net.biomodels.jummp.core.model.PublicationDetailExtractionContext as PDEC
+import net.biomodels.jummp.core.model.PublicationLinkProviderTransportCommand as PLPTC
 import net.biomodels.jummp.core.model.RepositoryFileTransportCommand as RFTC
 import net.biomodels.jummp.core.model.audit.AccessFormat
 import net.biomodels.jummp.core.model.audit.AccessType
 import net.biomodels.jummp.deployment.biomodels.CurationNotesTransportCommand
 import net.biomodels.jummp.model.Model
-import net.biomodels.jummp.core.model.PublicationLinkProviderTransportCommand as PLPTC
 import net.biomodels.jummp.model.Revision
 import net.biomodels.jummp.plugins.security.PersonTransportCommand
 import net.biomodels.jummp.plugins.security.Team
@@ -977,30 +978,33 @@ About to submit ${mainFilesMap.inspect()} and ${additionalFilesMap.inspect()}.""
                 if (!params.PubLinkProvider && params.PublicationLink) {
                     flash.flashMessage = "Please select a publication link type."
                     return error()
-                }
-                Map<String,String> modifications = new HashMap<String,String>()
-                    if (params.PubLinkProvider) {// one of the publication link providers has been selected
-                        if (!publicationService.verifyLink(params.PubLinkProvider, params.PublicationLink)) {
+                } else {
+                    String pubLinkProvider = params.list("PubLinkProvider")[0]
+                    String pubLink = params.list("PublicationLink")[0]
+                    if (pubLinkProvider) { // one of the publication link providers has been selected
+                        if (!publicationService.verifyLink(pubLinkProvider, pubLink)) {
                             flash.flashMessage = "The link is not a valid ${params.PubLinkProvider}"
                             return error()
                         }
                         ModelTransportCommand model = flow.workingMemory.get("ModelTC") as ModelTransportCommand
                         boolean providerHasChanged = params.PubLinkProvider !=
-                                model.publication?.linkProvider?.linkType
+                            model.publication?.linkProvider?.linkType
                         boolean linkHasChanged = params.PublicationLink != model.publication?.link
                         if (providerHasChanged || linkHasChanged) {
+                            Map<String,String> modifications = new HashMap<String,String>()
                             modifications.put("PubLinkProvider", params.PubLinkProvider)
                             modifications.put("PubLink", params.PublicationLink)
                             submissionService.updatePublicationLink(flow.workingMemory, modifications)
                         } else {
                             // go through publication editor in any case
+                            flow.workingMemory.put("previousPubLinkProvider", model.publication?.linkProvider)
+                            flow.workingMemory.put("previousPubLink", model.publication?.link)
                             flow.workingMemory.put("RetrievePubDetails", true)
                         }
                         flow.workingMemory.put("SelectedPubLinkProvider", params.PubLinkProvider)
                     } else { // 'No publication available' has been chosen
-                        ModelTransportCommand model = flow.workingMemory.get('ModelTC') as ModelTransportCommand
                         RevisionTransportCommand revision = flow.workingMemory.get("RevisionTC") as RevisionTransportCommand
-                        def publication = revision.model.publication
+                        PublicationTransportCommand publication = revision.model.publication
                         if (publication) {
                             revision.model.publication = null
                             if (flow.workingMemory.containsKey("Authors")) {
@@ -1009,6 +1013,7 @@ About to submit ${mainFilesMap.inspect()} and ${additionalFilesMap.inspect()}.""
                         }
                         flow.workingMemory.put("RetrievePubDetails", false)
                     }
+                }
             }.to "getPublicationDataIfPossible"
             on("Cancel").to "cleanUpAndTerminate"
             on("Back").to "displayModelInfo"
@@ -1019,39 +1024,31 @@ About to submit ${mainFilesMap.inspect()} and ${additionalFilesMap.inspect()}.""
                     ModelTransportCommand model = flow.workingMemory.get("ModelTC") as ModelTransportCommand
                     if (model.publication.linkProvider) {
                         PublicationTransportCommand retrieved
-                        PublicationDetailExtractionContext publicationContext
+                        PDEC publicationContext
                         // Case: PUBMED, DOI, CUSTOM, MANUAL_ENTRY
                         if (flow.workingMemory.containsKey("publication_objects_in_working")) {
-                            def publicationMap = flow.workingMemory.get("publication_objects_in_working") as Map<Object, PublicationDetailExtractionContext>
+                            def publicationMap = flow.workingMemory.get("publication_objects_in_working") as Map<Object, PDEC>
                             publicationContext = publicationMap.get(params.PubLinkProvider)
-                            PLPTC previousPubLinkProvider = flow.workingMemory.get("previousPubLinkProvider") as PLPTC
-                            String previousPubLink = flow.workingMemory.get("previousPubLink")
-                            PLPTC updatedPubLinkProvider = publicationContext.publication.linkProvider
-                            boolean changedPubLinkProvider = previousPubLinkProvider.linkType != updatedPubLinkProvider.linkType
-                            boolean changedPubLink = previousPubLink != publicationContext.publication.link
-                            boolean changed =  changedPubLinkProvider || changedPubLink
-                            if (publicationContext.publication && !changed) {
-                                // reload the publication from cache
-                                retrieved = publicationContext.publication
-                                if (publicationContext.comesFromDatabase) {
-                                    flash.flashMessage = g.message(code: "publication.editor.duplicateEntry.message")
+                            if (publicationContext.publication) {
+                                PLPTC previousPubLinkProvider = flow.workingMemory.get("previousPubLinkProvider") as PLPTC
+                                String previousPubLink = flow.workingMemory.get("previousPubLink")
+                                PLPTC updatedPubLinkProvider = publicationContext.publication?.linkProvider
+                                boolean changedPubLinkProvider = previousPubLinkProvider?.linkType != updatedPubLinkProvider?.linkType
+                                boolean changedPubLink = previousPubLink != publicationContext?.publication?.link
+                                boolean changed =  changedPubLinkProvider || changedPubLink
+                                if (!changed) {
+                                    // reload the publication from cache
+                                    retrieved = publicationContext.publication
+                                    if (publicationContext.comesFromDatabase) {
+                                        flash.flashMessage = g.message(code: "publication.editor.duplicateEntry.message")
+                                    }
+                                } else { // load from database, external call or create a default PTC
+                                    publicationContext = loadOrFetchOrCreatePublication(model)
+                                    retrieved = publicationContext.publication
                                 }
                             } else { // load from database, external call or create a default PTC
-                                try {
-                                    publicationContext = publicationService.getPublicationExtractionContext(model.publication)
-                                    if (publicationContext.publication) {
-                                        retrieved = publicationContext.publication
-                                        if (publicationContext.comesFromDatabase) {
-                                            flash.flashMessage = g.message(code: "publication.editor.duplicateEntry.message")
-                                        }
-                                    } else {
-                                        retrieved = publicationService.createPTCWithMinimalInformation(params.PubLinkProvider, params.PublicationLink, [])
-                                        publicationContext.comesFromDatabase = false
-                                    }
-                                }
-                                catch (Exception e) {
-                                    log.error(e.message, e)
-                                }
+                                publicationContext = loadOrFetchOrCreatePublication(model)
+                                retrieved = publicationContext.publication
                             }
                         } else {
                             log.error("Expected publication objects initialised in workingMemory.")
@@ -1062,7 +1059,7 @@ About to submit ${mainFilesMap.inspect()} and ${additionalFilesMap.inspect()}.""
                             flow.workingMemory.put("Authors", model.publication.authors)
 
                             // Update the publication objects in working
-                            def publicationMap = flow.workingMemory.get("publication_objects_in_working") as Map<Object, PublicationDetailExtractionContext>
+                            def publicationMap = flow.workingMemory.get("publication_objects_in_working") as Map<Object, PDEC>
                             publicationContext.publication = retrieved
                             publicationMap.put(params.PubLinkProvider, publicationContext)
                         }
@@ -1073,8 +1070,7 @@ About to submit ${mainFilesMap.inspect()} and ${additionalFilesMap.inspect()}.""
                     }
                     conversation.changesMade.add("Amended publication details")
                     publicationInfoPage()
-                }
-                else {
+                } else {
                     displaySummaryOfChanges()
                 }
             }
@@ -1086,9 +1082,8 @@ About to submit ${mainFilesMap.inspect()} and ${additionalFilesMap.inspect()}.""
             on("Continue"){
                 ModelTransportCommand model =
                             flow.workingMemory.get("ModelTC") as ModelTransportCommand
-                def publicationMap = flow.workingMemory.get("publication_objects_in_working") as
-                    Map<Object, PublicationDetailExtractionContext>
-                PublicationDetailExtractionContext pubContext = publicationMap.get(flow.workingMemory.get("SelectedPubLinkProvider"))
+                def publicationMap = flow.workingMemory.get("publication_objects_in_working") as Map<Object, PDEC>
+                PDEC pubContext = publicationMap.get(flow.workingMemory.get("SelectedPubLinkProvider"))
                 PublicationTransportCommand tempPTC = pubContext.publication
                 bindData(tempPTC, params, [exclude: ['authors']])
                 bindData(model.publication, params, [exclude: ['authors']])
@@ -1244,6 +1239,26 @@ Errors: ${model.publication.errors.allErrors.inspect()}."""
         abort()
         displayConfirmationPage()
         displayErrorPage()
+    }
+
+    private PDEC loadOrFetchOrCreatePublication(ModelTransportCommand modelTC) {
+        try {
+            PDEC publicationContext = publicationService.getPublicationExtractionContext(modelTC.publication)
+            if (publicationContext.publication) {
+                if (publicationContext.comesFromDatabase) {
+                    flash.flashMessage = g.message(code: "publication.editor.duplicateEntry.message")
+                }
+            } else {
+                PublicationTransportCommand retrieved
+                retrieved = publicationService.createPTCWithMinimalInformation(params.PubLinkProvider, params.PublicationLink, [])
+                publicationContext.publication = retrieved
+                publicationContext.comesFromDatabase = false
+            }
+            return publicationContext
+        } catch (Exception e) {
+            log.error(e.message, e)
+            return null
+        }
     }
 
     private void serveModelAsCombineArchive(List<RFTC> files, def resp) {
