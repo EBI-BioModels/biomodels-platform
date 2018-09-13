0a9ea4d07020ff944b251f6bc327e86e767d4814
Mihai Glont <mihai.glont@gmail.com>
2018-09-13 13:44:32 +0100

    JBM-276,JBM-311 added db indices for fields used by idGenerator initialisers

--- grails-app/migrations/changelog.groovy
+++ grails-app/migrations/changelog.groovy
@@ -1265,5 +1265,7 @@ databaseChangeLog = {
     include file: '20180515_modelDeleted_personName_idx.groovy'
 
     include file: '20180517_create_model_class.groovy'
+
+    include file: '20180913_indexRevisionDateAndNumber.groovy'
 }
 
