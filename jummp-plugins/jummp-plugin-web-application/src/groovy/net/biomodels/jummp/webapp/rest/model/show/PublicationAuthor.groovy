88db201ef33b93f1e814acd8f97fc207f3b56cb2
Tung Nguyen <nvntung@gmail.com>
2018-12-07 17:11:09 +0000

    JBM-322 rename the property

--- jummp-plugins/jummp-plugin-web-application/src/groovy/net/biomodels/jummp/webapp/rest/model/show/PublicationAuthor.groovy
+++ jummp-plugins/jummp-plugin-web-application/src/groovy/net/biomodels/jummp/webapp/rest/model/show/PublicationAuthor.groovy
@@ -3,12 +3,12 @@ package net.biomodels.jummp.webapp.rest.model.show
 import net.biomodels.jummp.plugins.security.PersonTransportCommand
 
 class PublicationAuthor {
-    String userRealName
+    String name
     String institution
     String orcid
 
     PublicationAuthor(PersonTransportCommand personTC) {
-        userRealName = personTC.userRealName
+        name = personTC.userRealName
         institution = personTC.institution
         orcid = personTC.orcid
     }
