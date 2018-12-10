2b3f8f6a74c8e97877936410ee8a219ad68eb2c2
Tung Nguyen <nvntung@gmail.com>
2018-12-10 15:49:40 +0000

    JBM-302 update security settings to allow curators to access curation tasks

--- jummp-plugins/jummp-plugin-web-application/grails-app/controllers/net/biomodels/jummp/webapp/SearchController.groovy
+++ jummp-plugins/jummp-plugin-web-application/grails-app/controllers/net/biomodels/jummp/webapp/SearchController.groovy
@@ -185,12 +185,12 @@ class SearchController {
         respond new SearchResults(results)
     }
 
-    @Secured(['ROLE_ADMIN'])
+    @Secured(['ROLE_ADMIN', 'ROLE_CURATOR'])
     def regen() {
         render(view: "regen")
     }
 
-    @Secured(['ROLE_ADMIN'])
+    @Secured(['ROLE_ADMIN', 'ROLE_CURATOR'])
     def regenIndices() {
         long start = System.currentTimeMillis()
         searchService.regenerateIndices()
