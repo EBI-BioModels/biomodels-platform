740e2fbb8ce445b0e5784e0748487c4d8cfa4816
chinmay_space <ak.chinmay@gmail.com>
2019-02-18 12:04:11 +0000

    BP-67 : Added Column Organism in the Ebi Search URL
    - In the fields list in Ebi Search the organism field is now included

--- jummp-plugins/jummp-plugin-biomodels-dom/src/groovy/net/biomodels/jummp/deployment/biomodels/parameters/ParameterSearchCommand.groovy
+++ jummp-plugins/jummp-plugin-biomodels-dom/src/groovy/net/biomodels/jummp/deployment/biomodels/parameters/ParameterSearchCommand.groovy
@@ -13,7 +13,7 @@ class ParameterSearchCommand {
     public static final String DEFAULT_QUERY = '*:*'
     public static
     final String BASE_URL = "https://wwwdev.ebi.ac.uk/ebisearch/ws/rest/biomodels_parameters?format=json" +
-        "&fields=entity_RAW,entity_id,initial_data_RAW,reaction_RAW,model,publication,rate_RAW,parameters_RAW,entity_accession_url,reaction_sbo_term_link,entity_sbo_term_link"
+        "&fields=entity_RAW,entity_id,initial_data_RAW,reaction_RAW,model,organism,publication,rate_RAW,parameters_RAW,entity_accession_url,reaction_sbo_term_link,entity_sbo_term_link"
     String query
     Integer size
     Integer start
