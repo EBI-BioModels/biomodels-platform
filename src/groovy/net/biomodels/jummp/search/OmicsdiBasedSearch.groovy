e5bb0fca9533e299d530320b9bed2e843a872d77
Tung Nguyen <nvntung@gmail.com>
2019-05-08 13:49:08 +0100

    disable transactional from searchModels method
    Bots or programmactical access potentially cause the issue
    "Too many connections" that hits against the database.

--- src/groovy/net/biomodels/jummp/search/OmicsdiBasedSearch.groovy
+++ src/groovy/net/biomodels/jummp/search/OmicsdiBasedSearch.groovy
@@ -24,17 +24,14 @@
 
 package net.biomodels.jummp.search
 
+import grails.transaction.NotTransactional
 import grails.util.Environment
 import grails.util.Holders
 import groovy.json.JsonBuilder
 import net.biomodels.jummp.annotationstore.ResourceReference
 import net.biomodels.jummp.core.ModelSearchStrategy
 import net.biomodels.jummp.core.events.ModelOperationEvent
-import net.biomodels.jummp.core.model.ModelFormatTransportCommand
-import net.biomodels.jummp.core.model.ModelState
-import net.biomodels.jummp.core.model.ModelTransportCommand
-import net.biomodels.jummp.core.model.PublicationTransportCommand
-import net.biomodels.jummp.core.model.RevisionTransportCommand
+import net.biomodels.jummp.core.model.*
 import net.biomodels.jummp.core.model.identifier.ModelIdentifierUtils
 import net.biomodels.jummp.model.Revision
 import org.apache.commons.logging.Log
@@ -150,6 +147,7 @@ class OmicsdiBasedSearch implements ModelSearchStrategy, ApplicationListener<Mod
         out.toString()
     }
 
+    @NotTransactional
     SearchResponse searchModels(String query, SortOrder sortOrder,
             Map<String, Integer> paginationCriteria = ["start": 0, "length": 50, "facetCount": 10] ) {
         long start = System.currentTimeMillis()
