f742f7d272d32cc9fc00ef1eb0cd2967b2f9a382
chinmay_space <ak.chinmay@gmail.com>
2019-06-19 09:48:12 +0100

    BP-87 : Changed the expectedRateShow value in ParameterSearchResultSpec due to inline function conversion change
    - Due to conversion of inline functions to resolved equations the expectedRateShow value had to be changed to meet with newly indexed data

--- jummp-plugins/jummp-plugin-biomodels-dom/test/unit/net/biomodels/jummp/deployment/biomodels/ParameterSearchResultsSpec.groovy
+++ jummp-plugins/jummp-plugin-biomodels-dom/test/unit/net/biomodels/jummp/deployment/biomodels/ParameterSearchResultsSpec.groovy
@@ -36,6 +36,7 @@ class ParameterSearchResultsSpec extends Specification {
         String expectedEntityShow = "<a target='_blank' href='http://identifiers.org/pubchem.compound/122357' > 122357 </a><hr/><span class='legend-green'>E4P</span>"
         String expectedRateShow = "chloroplast*function_7(Vm, [668], [122357], [sedoheptulose 1,7-bisphosphate], q, Ks1, Ks2)<hr/><span class='legend-green'>chloroplast*function_7(Vm, DHAP, E4P, SBP, q, Ks1, Ks2)</span>"
 
+        String expectedRateShow = "chloroplast*Vm*([668]*[122357]-[sedoheptulose 1,7-bisphosphate]/q)/(([668]+Ks1)*([122357]+Ks2))<hr/><span class='legend-green'>chloroplast*Vm*(DHAP*E4P-SBP/q)/((DHAP+Ks1)*(E4P+Ks2))</span>"
 
         results.entries.find{value ->
            value.fields.entity_id == expectedEntityId &&
