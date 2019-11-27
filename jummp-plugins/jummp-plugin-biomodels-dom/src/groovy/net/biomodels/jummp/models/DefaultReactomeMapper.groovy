e7c8d0718eeb95a9d1da2c717220d62327c080e0
chinmay_space <ak.chinmay@gmail.com>
2019-11-27 18:41:10 +0000

    JBM-348 : Added ReactomeMapper interface and its implmentation DefaultReactomeMapper
    - Added ReactomeMapper interface and its implmentation DefaultReactomeMapper
    this holds the business logic creating map of modelIds and reactomeIds out of
    the model2pathway file

---
+++ jummp-plugins/jummp-plugin-biomodels-dom/src/groovy/net/biomodels/jummp/models/DefaultReactomeMapper.groovy
@@ -0,0 +1,42 @@
+package net.biomodels.jummp.models
+/**
+ * @author carankalle on 26/11/2019.
+ */
+class DefaultReactomeMapper implements ReactomeMapper {
+    Map<String, String> modelPathwayMap = new HashMap<String, String>()
+
+    Map<String, String> getModelPathwayMap() {
+        return modelPathwayMap
+    }
+
+    DefaultReactomeMapper() {
+        extractModel2PathwayData()
+    }
+
+    @Override
+    void extractModel2PathwayData() {
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("./models2pathways.tsv")
+        println(inputStream)
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"))
+        try  {
+            for(String line : reader.lines().iterator()) {
+                parseLineAndPrepareMap(line)
+            }
+        } catch (IOException ie) {
+            System.err.printf("Unable to parse reactome mapping file, %s", ie.getMessage())
+        } finally{
+            print("executed....")
+            inputStream.close()
+            reader.close()
+        }
+    }
+
+    private void parseLineAndPrepareMap(String line) {
+        String[] lineArr = line.split('\t')
+        if (!modelPathwayMap.containsKey(lineArr[0])) {
+            modelPathwayMap[lineArr[0]] = lineArr[1]
+        }
+
+    }
+
+}
---
+++ jummp-plugins/jummp-plugin-biomodels-dom/src/groovy/net/biomodels/jummp/models/ReactomeMapper.groovy
@@ -0,0 +1,9 @@
+package net.biomodels.jummp.models
+
+/**
+ * @author carankalle on 26/11/2019.
+ */
+interface ReactomeMapper {
+    void extractModel2PathwayData()
+}
+
