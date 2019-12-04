417d000d6b2ff72155e0a538b160c992956245ea
chinmay_space <ak.chinmay@gmail.com>
2019-12-04 11:47:08 +0000

    JBM-348 : Added doWithSpring config
    - Added doWithSpring configuration
    - Calling reactomeMapperFactoryBean directly to get the ReactomeMapper object

--- jummp-plugins/jummp-plugin-biomodels-dom/test/unit/net/biomodels/jummp/deployment/biomodels/ReactomeMapperFactoryBeanSpec.groovy
+++ jummp-plugins/jummp-plugin-biomodels-dom/test/unit/net/biomodels/jummp/deployment/biomodels/ReactomeMapperFactoryBeanSpec.groovy
@@ -32,7 +32,7 @@ package net.biomodels.jummp.deployment.biomodels
 
 import grails.test.mixin.TestMixin
 import grails.test.mixin.support.GrailsUnitTestMixin
-import net.biomodels.jummp.models.DefaultReactomeMapper
+import net.biomodels.jummp.models.ReactomeMapper
 import net.biomodels.jummp.models.ReactomeMapperFactoryBean
 import spock.lang.Specification
 
@@ -42,17 +42,13 @@ import spock.lang.Specification
 @TestMixin(GrailsUnitTestMixin)
 class ReactomeMapperFactoryBeanSpec extends Specification {
 
-    def "test extraction of reactome pathway mapped to models" () {
-        defineBeans {
-            defaultReactomeMapper(DefaultReactomeMapper) { bean ->
-                bean.scope = 'prototype'
-            }
-        }
-        ReactomeMapperFactoryBean reactomeFactory = new ReactomeMapperFactoryBean()
-        reactomeFactory.setApplicationContext(applicationContext)
+    static doWithSpring = {
+        reactomeMapperFactoryBean(ReactomeMapperFactoryBean)
+    }
 
-        when: 'we request reactome mapper bean'
-        def reactomeMapper = reactomeFactory.getObject()
+    def "test extraction of reactome pathway mapped to models" () {
+        when: 'we request reactome mapper factory bean'
+        ReactomeMapper reactomeMapper = grailsApplication.mainContext.getBean("reactomeMapperFactoryBean") as ReactomeMapper
         then: 'reactome data extracted correctly'
         reactomeMapper.getModelPathwayMap().size() > 0
     }
