a297905f829fd03cb4cf71e25f4cd7361a6dd216
chinmay_space <ak.chinmay@gmail.com>
2019-12-05 15:15:52 +0000

    JBM-348 : Created DummyReactomeServiceFactoryBean to make mocking
    - Mocked the ReactomeServiceFactoryBean with DummyReactomeServiceFactoryBean

--- jummp-plugins/jummp-plugin-biomodels-dom/test/unit/net/biomodels/jummp/deployment/biomodels/ReactomeServiceSpec.groovy
+++ jummp-plugins/jummp-plugin-biomodels-dom/test/unit/net/biomodels/jummp/deployment/biomodels/ReactomeServiceSpec.groovy
@@ -1,25 +1,51 @@
 package net.biomodels.jummp.deployment.biomodels
 
-import grails.test.mixin.TestFor
+import grails.test.mixin.TestMixin
+import grails.test.mixin.support.GrailsUnitTestMixin
+import net.biomodels.jummp.models.ReactomeServiceFactoryBean
 
 /**
- * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ * @author carankalle on 26/11/2019.
+ * @author mglont on 05/12/2019.
  */
-@TestFor(ReactomeService)
+@TestMixin(GrailsUnitTestMixin)
 class ReactomeServiceSpec {
+    private final String reactomeId = "R-HSA-1250196"
+    private final String modelId = "BIOMD0000000255"
+    private final Map<String, String> reactomeMapping = [
+        modelId: reactomeId
+    ]
 
-    def reactomeService
-
-    void "test getPathwayForModelId"() {
-        when: "Asked for reactome Id for BIOMD0000000255"
-
-        def service = mockFor(ParameterSearchService)
-        service.demand.getPathwayForModelId { String modelId ->
-            return "R-HSA-1250196"
+    def doWithSpring = {
+        reactomeService(DummyReactomeServiceFactoryBean, reactomeMapping) {
+            it.scope = 'prototype'
         }
-        reactomeService = service.createMock()
-        String reactomeId = reactomeService.getPathwayForModelId("BIOMD0000000255")
+    }
+    void "test getPathwayForModelId"() {
+
+        when: "Asked for reactome Id for a known model"
+        def service = grailsApplication.mainContext.reactomeService
+
+        String actualReactomeId = service.getPathwayForModelId(modelId)
         then: "reactomeId string shouldn't be empty"
-        !reactomeId.isEmpty() && null != reactomeId
+        actualReactomeId == reactomeId
+
+        and: "asking for a pathway id for a model that does not exist should return null"
+        service.getPathwayForModelId("THIS-DOES-NOT-EXIST") == null
+        service.getPathwayForModelId("") == null
+        service.getPathwayForModelId(null) == null
+    }
+}
+
+class DummyReactomeServiceFactoryBean extends ReactomeServiceFactoryBean {
+    final Map<String, String> mockModelPathwayMapping
+
+    DummyReactomeServiceFactoryBean(Map<String, String> mapping) {
+        this.mockModelPathwayMapping = mapping
+    }
+
+    @Override
+    ReactomeService getObject() throws Exception {
+        new ReactomeService(mockModelPathwayMapping)
     }
 }
