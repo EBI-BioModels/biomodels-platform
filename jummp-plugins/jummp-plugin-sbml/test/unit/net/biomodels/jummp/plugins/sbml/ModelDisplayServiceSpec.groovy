da33a0b3b421e62f268488a0b3790c866ff2371a
chinmay_space <ak.chinmay@gmail.com>
2020-01-30 16:35:38 +0000

    feature/JBM-466 : Added ModelDisplayServiceSpec
    - Added tests for ModelDisplayService which tests the retrieval of model components
    as of now Species and Reactions

---
+++ jummp-plugins/jummp-plugin-sbml/test/unit/net/biomodels/jummp/plugins/sbml/ModelDisplayServiceSpec.groovy
@@ -0,0 +1,51 @@
+package net.biomodels.jummp.plugins.sbml
+
+import com.google.gson.JsonObject
+import grails.test.mixin.TestMixin
+import grails.test.mixin.services.ServiceUnitTestMixin
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestMixin(ServiceUnitTestMixin)
+class ModelDisplayServiceSpec extends Specification {
+    ModelDisplayService service
+
+    void setup() {
+        service = applicationContext.modelDisplayService
+    }
+
+    static doWithSpring = {
+        modelDisplayService(ModelDisplayService) { bean ->
+            accessUrl = "http://localhost:8887/api/access"
+        }
+    }
+    void 'service instances are wired correctly'() {
+        expect:
+        service.accessUrl == 'http://localhost:8887/api/access'
+    }
+
+    void "test getComponentsFromModelDisplay"() {
+
+        when: "The service method's getComponentsFromModelDisplay is called"
+        def result = service.getComponentsFromModelDisplay("BIOMD0000000001",0,10,"species")
+
+        then: "it should contain records"
+        result.species.size() > 0
+
+        then: "it should contain correct records"
+        List<JsonObject> species = result.species as List<JsonObject>
+        species.find() {value->
+            (value.get("speciesId") == "BLL")
+        }
+
+        when: "Tested with reactions"
+        result = service.getComponentsFromModelDisplay("BIOMD0000000001",0,10,"reactions")
+
+        then: "it should have atleast one reaction"
+        result.reactions.size() > 0
+
+
+    }
+}
