990f218d1aaf86dd447c7af55dab3c661339042c
Tung Nguyen <nvntung@gmail.com>
2020-02-04 16:08:03 +0000

    JBM-479 insert the copyright statement

--- jummp-plugins/jummp-plugin-common-format/grails-app/controllers/net/biomodels/jummp/plugins/format/CommonFormatController.groovy
+++ jummp-plugins/jummp-plugin-common-format/grails-app/controllers/net/biomodels/jummp/plugins/format/CommonFormatController.groovy
@@ -1,5 +1,33 @@
+/**
+ * Copyright (C) 2010-2020 EMBL-European Bioinformatics Institute (EMBL-EBI),
+ * Deutsches Krebsforschungszentrum (DKFZ)
+ *
+ * This file is part of Jummp.
+ *
+ * Jummp is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * Jummp is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with Jummp; if not, see <http://www.gnu.org/licenses/agpl-3.0.html>.
+ */
+
 package net.biomodels.jummp.plugins.format
 
+/**
+ * <p>Controls the way of rendering format specific views</p>
+ * <p style="font-weight: bold">Authors:</p>
+ * <ul>
+ *   <li>Mihai Glonț&nbsp;<a href="mailto:mihai.glont@ebi.ac.uk">mihai.glont@ebi.ac.uk</a></li>
+ *   <li>Tung Nguyen&nbsp;<a href="mailto:tung.nguyen@ebi.ac.uk">tung.nguyen@ebi.ac.uk</a></li>
+ *  </ul>
+ */
 class CommonFormatController {
     def show() {
 
