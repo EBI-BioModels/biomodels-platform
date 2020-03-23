17d3d0ed9fd9d97aede4dff7011f684df946fee1
Mihai Glont <mihai.glont@gmail.com>
2020-03-23 18:44:00 +0000

    JBM-490 fix query for My Models page
    SQL requires that columns used in the ORDER BY clause must also
    appear in the SELECT clause. This commit ensures that the query
    we perform for retrieving the content for the My models page complies
    with this requirement.
    
    I also added an integration test.

--- grails-app/services/net/biomodels/jummp/core/ModelService.groovy
+++ grails-app/services/net/biomodels/jummp/core/ModelService.groovy
@@ -253,7 +253,11 @@ class ModelService {
         }
         def results = []
         try {
-            results = Model.getAll(Model.executeQuery(query, namedParams, metaParams))
+            List queryResultSet = Model.executeQuery(query, namedParams, metaParams)
+            // result set consists of [model, <sortColumnValue>, revisionNumber] and we can't avoid it
+            // extract the first column before the result set gets used downstream
+            List<Long> modelIds = queryResultSet.collect { it[0] }
+            results = Model.getAll(modelIds)
         } catch (Exception e) {
             log.error("Exception $e while executing $query with '$namedParams' (page '$metaParams')")
         }
@@ -264,8 +268,11 @@ class ModelService {
                                          boolean filterIsValid, String type,
                                          List filteredFormats, List filteredUsers,
                                          String sortingDirection, boolean isAdmin = false) {
+        String orderByColumn = getSortColumnAsString(sortColumn)
+        // we have to include $orderByColumn and revisionNumber in the selected columns
+        // in order for the query to be valid SQL
         String query = """\
-SELECT distinct m.id
+SELECT m.id, ${orderByColumn}, r.revisionNumber
 FROM Revision AS r RIGHT OUTER JOIN r.model AS m
 WHERE
     r.deleted = false
@@ -305,7 +312,7 @@ AND r.state = '${ModelState.UNPUBLISHED}'))"""
                 }
                 break
         }
-        query = """$query ORDER BY ${getSortColumnAsString(sortColumn)} ${sortingDirection}, r.revisionNumber desc"""
+        query = """$query ORDER BY ${orderByColumn} ${sortingDirection}, r.revisionNumber desc"""
         return query
     }
 
