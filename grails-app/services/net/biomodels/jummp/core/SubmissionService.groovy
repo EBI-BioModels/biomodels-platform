4433e2fcb57afceed0736915f3ea612ef1d0292a
Tung Nguyen <nvntung@gmail.com>
2021-09-17 21:42:30 +0100

    fix a typo

--- grails-app/services/net/biomodels/jummp/core/SubmissionService.groovy
+++ grails-app/services/net/biomodels/jummp/core/SubmissionService.groovy
@@ -735,7 +735,7 @@ class SubmissionService {
             Map info = ["name": name, "description": description, "modellingApproach": modellingApproach]
             Date stop = new Date()
             TimeDuration td = TimeCategory.minus(stop, start)
-            logger.debug("Detecting the model info ${info} of the model mail file ${modelFile.absolutePath} took $td")
+            logger.debug("Detecting the model info ${info} of the model main file ${modelFile.absolutePath} took $td")
             return info
         }
 
