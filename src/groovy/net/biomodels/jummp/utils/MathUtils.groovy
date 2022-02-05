c7566289c86c1f88b943f1bd821f5ec3cd0c0e1e
Tung Nguyen <nvntung@gmail.com>
2022-02-05 10:38:44 +0000

    JBM-512 add the licence information

--- src/groovy/net/biomodels/jummp/utils/MathUtils.groovy
+++ src/groovy/net/biomodels/jummp/utils/MathUtils.groovy
@@ -1,7 +1,41 @@
+/**
+ * Copyright (C) 2010-2022 EMBL-European Bioinformatics Institute (EMBL-EBI),
+ * Deutsches Krebsforschungszentrum (DKFZ)
+ *
+ * This file is part of Jummp.
+ *
+ * Jummp is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * Jummp is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with Jummp; if not, see <http://www.gnu.org/licenses/agpl-3.0.html>.
+ *
+ * Additional permission under GNU Affero GPL version 3 section 7
+ *
+ * If you modify Jummp, or any covered work, by linking or combining it with
+ * Apache Commons, Perf4j (or a modified version of that library), containing parts
+ * covered by the terms of Apache License v2.0, the licensors of this
+ * Program grant you additional permission to convey the resulting work.
+ *{Corresponding Source for a non-source form of such a combination shall
+ * include the source code for the parts of Apache Commons, Perf4j used as well as
+ * that of the covered work.}
+ **/
+
 package net.biomodels.jummp.utils
 
 import java.util.concurrent.ThreadLocalRandom
 
+/**
+ * Mathematical utilities are commonly used
+ * @author Tung Nguyen, nvntung@gmail.com
+ */
 class MathUtils {
     static final int rand(int min, int max) {
         return ThreadLocalRandom.current().nextInt(min, max + 1);
