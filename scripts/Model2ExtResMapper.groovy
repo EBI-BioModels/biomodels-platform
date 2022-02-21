6fd80175a2bbe44bfa39a3bd2bd2d4335af8aa11
Tung Nguyen <nvntung@gmail.com>
2022-02-21 15:03:34 +0000

    JBM-566 restructure the script to have different running configurations
    Prioritise to run against large models to avoid OutOfMemoryError

--- scripts/Model2ExtResMapper.groovy
+++ scripts/Model2ExtResMapper.groovy
@@ -38,6 +38,55 @@ class BioModels2ExternalResourcesMapper {
 
     private static final Logger logger = LoggerFactory.getLogger(BioModels2ExternalResourcesMapper.class)
 
+    List<Revision> queryQualifiedRevision() {
+        String query = """SELECT R1 FROM Revision AS R1 JOIN R1.model as M \
+WHERE \
+	M.deleted = false AND \
+	M.firstPublished != NULL AND \
+ 	R1.revisionNumber = (SELECT MAX(R2.revisionNumber) FROM Revision AS R2 \
+                         WHERE R2.model = M and R2.state = '${ModelState.PUBLISHED}') \
+ORDER BY M.id"""
+        List revisions = Revision.executeQuery(query)
+        revisions
+    }
+
+    void processData(def mdDS, final Revision revision) {
+        try {
+            long revisionId = revision.id
+            String pubOrDoiId = null
+            Publication publication = revision.model.publication
+            PLP.LinkType linkType = publication?.linkProvider?.linkType
+            if (linkType != PLP.LinkType.MANUAL_ENTRY && publication != null) {
+                pubOrDoiId = publication.link
+            }
+            List<EATC> annotations = mdDS.fetchAnnotations(revisionId)
+            String EXT_RES = System.getenv("EXT_RES")
+            if (!(EXT_RES in ["chebi", "ensembl", "uniprot"])) {
+                // set "uniprot" as the default if the external source hasn't been specified
+                EXT_RES = "uniprot"
+            }
+            List candidateList = annotations.findAll {
+                it.statement.object.datatype == EXT_RES
+            }
+            List iDList = candidateList.collect {
+                it.statement.object.accession
+            }.unique()
+            String modelIdentifier = revision.model.publicationId ?: revision.model.submissionId
+            iDList.each { String resourceId ->
+                println "${modelIdentifier}\t${resourceId}\t${pubOrDoiId}"
+            }
+        } catch (Exception e) {
+            String message = """\
+Cannot extract the model: ${revision.model.submissionId}, revision: ${revisionId} because of the below error:
+${e}"""
+            logger.error(message)
+            println(message)
+            e.printStackTrace()
+        } finally {
+            ctx.persistenceInterceptor?.destroy()
+        }
+    }
+
     void build() {
         String duration
         Instant startTime = Instant.now()
@@ -45,54 +94,15 @@ class BioModels2ExternalResourcesMapper {
         def mdDS = ctx.getBean("metadataDelegateService")
 
         try {
-            String query = """SELECT R1 FROM Revision AS R1 JOIN R1.model as M \
-WHERE \
-	M.deleted = false AND \
-	M.firstPublished != NULL AND \
- 	R1.revisionNumber = (SELECT MAX(R2.revisionNumber) FROM Revision AS R2 \
-                         WHERE R2.model = M and R2.state = '${ModelState.PUBLISHED}') \
-ORDER BY M.id"""
-            List revisions = Revision.executeQuery(query)
+            List revisions = queryQualifiedRevision()
             /*int start = 20
             int stop = 61
             revisions = revisions.subList(start, stop)*/
             final int POOL_SIZE = 8
             GParsPool.withPool(POOL_SIZE) {
                 revisions.eachParallel { Revision revision ->
-                    long revisionId = revision.id
                     ctx.persistenceInterceptor?.init()
-                    try {
-                        String pubOrDoiId = null
-			Publication publication = revision.model.publication
-                        PLP.LinkType linkType = publication?.linkProvider?.linkType
-                        if (linkType != PLP.LinkType.MANUAL_ENTRY && publication != null) {
-                            pubOrDoiId = publication.link
-                        }
-                        List<EATC> annotations = mdDS.fetchAnnotations(revisionId)
-                        String EXT_RES = System.getenv("EXT_RES")
-                        if (!(EXT_RES in ["chebi", "ensembl", "uniprot"])) {
-                            // set "uniprot" as the default if the external source hasn't been specified
-                            EXT_RES = "uniprot"
-                        }
-                        List candidateList = annotations.findAll {
-                            it.statement.object.datatype == EXT_RES
-                        }
-                        List iDList = candidateList.collect {
-                            it.statement.object.accession
-                        }.unique()
-                        String modelIdentifier = revision.model.publicationId ?: revision.model.submissionId
-                        iDList.each { String resourceId ->
-                            println "${modelIdentifier}\t${resourceId}\t${pubOrDoiId}"
-                        }
-                    } catch (Exception e) {
-                        String message = """\
-Cannot extract the model: ${revision.model.submissionId}, revision: ${revisionId} because of the below error:
-${e}"""
-                        logger.error(message)
-                        e.printStackTrace()
-                    } finally {
-                        ctx.persistenceInterceptor?.destroy()
-                    }
+                    processData(mdDS, revision)
                 }
             }
         } finally {
@@ -102,6 +112,47 @@ ${e}"""
             logger.info("Found and mapped the identifiers within $formattedDuration")
         }
     }
+
+    void retrieve() {
+        String duration
+        Instant startTime = Instant.now()
+        ctx.persistenceInterceptor?.init()
+        def mdDS = ctx.getBean("metadataDelegateService")
+
+        try {
+            File file = new File("scripts", "merged-uniprot-total-annotations-largest.csv")
+            List lines = file.readLines()
+            List<String> modelsToBeProcessed = new ArrayList<>()
+            for (String line: lines) {
+                String[] tokenized = line.split(":")
+                String modelId = tokenized[0].trim()
+                modelsToBeProcessed.add(modelId)
+            }
+            List revisions = queryQualifiedRevision()
+            println revisions.size()
+            revisions = revisions.findAll { Revision revision ->
+                String modelId = revision.model.publicationId ?: revision.model.submissionId
+                modelId in modelsToBeProcessed
+            }
+            revisions.each {
+                println "${it.model.publicationId ?: it.model.submissionId}: ${it.id} : ${it.revisionNumber}"
+            }
+
+            final int POOL_SIZE = 4
+            GParsPool.withPool(POOL_SIZE) {
+                revisions.eachParallel { Revision revision ->
+                    ctx.persistenceInterceptor?.init()
+                    processData(mdDS, revision)
+                }
+            }
+        } finally {
+            ctx.persistenceInterceptor?.destroy()
+            duration = Duration.between(startTime, Instant.now())
+            String formattedDuration = duration.toString()
+            println("Found and mapped the identifiers within $formattedDuration")
+            logger.info("Found and mapped the identifiers within $formattedDuration")
+        }
+    }
 }
 
-new BioModels2ExternalResourcesMapper(ctx: ctx).build()
+new BioModels2ExternalResourcesMapper(ctx: ctx).retrieve()
