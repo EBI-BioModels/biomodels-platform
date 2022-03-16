c187c1eceb0e11d2839cbef1600ac21729c64ee3
Tung Nguyen <nvntung@gmail.com>
2022-03-16 21:56:34 +0000

    JBM-81 stop printing the entire properties due to polluting the log file

--- grails-app/migrations/20191122_updateTableModellingApproach.groovy
+++ grails-app/migrations/20191122_updateTableModellingApproach.groovy
@@ -39,14 +39,15 @@ databaseChangeLog = {
                         model.modellingApproach = approach
                         if (model.save(flush: true)) {
                             println """\
-set modelling approach ${approach.properties} to model ${model.properties} successfully"""
+set modelling approach (${approach.accession}: ${approach.name}) to model ${model.submissionId} successfully"""
                         } else {
                             String msg = """\
-cannot update modelling apporach ${approach.properties} to model ${model.properties}"""
+cannot update modelling apporach (${approach.accession}: ${approach.name}) to model ${model.submissionId} \
+due to the errors: ${model.getErrors().toString()}"""
                             throw new IllegalStateException(msg)
                         }
                     } else {
-                        println "cannot find any MAMO term describing a modelling approach"
+                        println "cannot find any MAMO term describing a modelling approach for model ${model.submissionId}"
                     }
 
                     // clear session and save records after every 100 entries created
