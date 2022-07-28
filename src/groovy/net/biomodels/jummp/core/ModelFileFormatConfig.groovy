5b823e9f00ad42301dbc4ea4352ea3d05a3ca04d
Tung Nguyen <nvntung@gmail.com>
2022-07-28 07:27:26 +0200

    add the timestamp to the println statements

--- src/groovy/net/biomodels/jummp/core/ModelFileFormatConfig.groovy
+++ src/groovy/net/biomodels/jummp/core/ModelFileFormatConfig.groovy
@@ -58,7 +58,8 @@ class ModelFileFormatConfig {
 
     static void status() {
         String msgLog = "MODEL FILE FORMAT CONFIG STATUS:"
-        println("$msgLog ${services.size()} services and ${controllers.size()} controllers are available.")
+        println("""${new Date().format("yyyy-mm-dd HH:mm:ss")} $msgLog ${services.size()} \
+services and ${controllers.size()} controllers are available.""")
         msgLog += "\nSERVICES: ${services.inspect()}"
         msgLog += "\nCONTROLLERS: ${controllers.inspect()}"
         LOGGER.debug(msgLog)
