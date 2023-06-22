bc4027c178d0e3618a74772f2df2bafcac295015
Tung Nguyen <nvntung@gmail.com>
2023-06-23 00:11:37 +0100

    JBM-468 fix the issue: unable to resolve class

--- jummp-plugins/jummp-plugin-core-api/src/groovy/net/biomodels/jummp/core/constants/BioModels.groovy
+++ jummp-plugins/jummp-plugin-core-api/src/groovy/net/biomodels/jummp/core/constants/BioModels.groovy
@@ -1,6 +1,8 @@
 package net.biomodels.jummp.core.constants
 
 final class BioModels {
+    // the minimal number of cross references displayed or hidden at a time
+    static final int BM_MIN_NB_CR = 5
     static final String BM_ROOT_URL = "https://www.ebi.ac.uk/biomodels"
     static final String BM_PROD_SEARCH_URL_PREFIX = "$BM_ROOT_URL/search"
     static final String EBI_PROD_WS_REST_BM_URL = "https://www.ebi.ac.uk/ebisearch/ws/rest/biomodels"
