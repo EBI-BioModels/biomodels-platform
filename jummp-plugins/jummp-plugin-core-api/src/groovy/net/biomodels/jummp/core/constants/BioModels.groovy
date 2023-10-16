f92eebe92a09fde4a2e9fe05005c064e08024213
Tung Nguyen <nvntung@gmail.com>
2023-10-16 16:32:35 +0100

    JBM-572 move files into repo dir

--- jummp-plugins/jummp-plugin-core-api/src/groovy/net/biomodels/jummp/core/constants/BioModels.groovy
+++ jummp-plugins/jummp-plugin-core-api/src/groovy/net/biomodels/jummp/core/constants/BioModels.groovy
@@ -8,7 +8,7 @@ final class BioModels {
     static final String BM_PROD_SEARCH_URL_PREFIX = "$BM_ROOT_URL/search"
     static final String EBI_PROD_WS_REST_BM_URL = "https://www.ebi.ac.uk/ebisearch/ws/rest/biomodels"
     static final String EBI_BMPROD_PUBLIC_FTP = "https://ftp.ebi.ac.uk/pub/databases/biomodels"
-    static final String EBI_BMDEV_PUBLIC_FTP = "https://ftp.ebi.ac.uk/pub/databases/biomodels/dev"
+    static final String EBI_BMDEV_PUBLIC_FTP = "https://ftp.ebi.ac.uk/pub/databases/biomodels/dev/repo"
 
     // the maximum file size or the total size allowed to upload and download by streaming directly via web interface
     static long MAX_FILE_SIZE = 500 * 1024 * 1024
