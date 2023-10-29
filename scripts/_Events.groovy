b8c70d51f5b5642d54b9a0ef085eb65da6f22d51
Tung Nguyen <nvntung@gmail.com>
2023-10-29 22:26:07 +0000

    get the right commit id
    Why?
    - The dev deployment is done after merging from any feature branch.
    - The prod deployment is done after merging from the k8sdev branch.
      Therefore, git log -2 on the dev deployment and git log -3 on the
      prod deployment.

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -25,12 +25,24 @@ includeTargets << new File("./scripts/WeceemExport.groovy")
  */
 eventCompileStart = { msg ->
     // the only way I could get Groovy to execute a command with an argument containing spaces
-    def cmd = ["git", "log", "-1",
-         "--pretty=format:<a href=\"//bitbucket.org/biomodels/jummp-biomodels/commits/all?search=%h\">%h</a> | %aD"]
+    def cmd = ["git", "rev-parse", "--abbrev-ref", "HEAD"]
     def proc = cmd.execute()
     proc.waitFor()
-    ant.mkdir(dir: "grails-app/views/templates/")
     String txt = proc.in.text
+    println "branch: $txt"
+    int skip = 0
+    if (txt == "k8sdev") {
+        skip = 1
+    } else if ("k8sprod" == txt) {
+        skip = 2
+    }
+    cmd = ["git", "log", "-1", "--skip=${skip}",
+         "--pretty=format:<a href=\"//bitbucket.org/biomodels/jummp-biomodels/commits/all?search=%h\">%h</a> | %aD"]
+    proc = cmd.execute()
+    proc.waitFor()
+    ant.mkdir(dir: "grails-app/views/templates/")
+    txt = proc.in.text
+    println "commit id: $txt"
     new FileOutputStream("grails-app/views/templates/_version.gsp", false) << txt
 
     // copy the messages.properties
