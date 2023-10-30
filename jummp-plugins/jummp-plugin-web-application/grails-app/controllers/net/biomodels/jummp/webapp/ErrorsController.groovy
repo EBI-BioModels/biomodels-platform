7e5dcac400488f208a57a0ce590c29b7617b816c
Tung Nguyen <nvntung@gmail.com>
2023-10-30 13:56:03 +0000

    hardcode with an explicit code

--- jummp-plugins/jummp-plugin-web-application/grails-app/controllers/net/biomodels/jummp/webapp/ErrorsController.groovy
+++ jummp-plugins/jummp-plugin-web-application/grails-app/controllers/net/biomodels/jummp/webapp/ErrorsController.groovy
@@ -37,7 +37,7 @@ class ErrorsController {
     def error400() {
         response.status = HttpServletResponse.SC_BAD_REQUEST
         withFormat {
-            html { [resource: request.forwardURI, errorDescription: params?.errorDescription, code: response.status] }
+            html { [resource: request.forwardURI, errorDescription: params?.errorDescription, code: 400] }
             '*' { respond getError("400", [request.forwardURI]) }
         }
     }
@@ -45,7 +45,7 @@ class ErrorsController {
     def error403() {
         response.setStatus HttpServletResponse.SC_FORBIDDEN
         withFormat {
-            html { [authenticated: springSecurityService.isLoggedIn(), code: response.status] }
+            html { [authenticated: springSecurityService.isLoggedIn(), code: 403] }
             '*' { respond getError("403") }
         }
     }
