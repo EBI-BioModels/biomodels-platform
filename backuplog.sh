73ccdcb45df220b5953e0b20d792883385609c1a
Tung Nguyen <nvntung@gmail.com>
2023-10-31 22:18:08 +0000

    create a bash script to back up all logs before redeploying

---
+++ backuplog.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+export KUBECONFIG=~/.kube/hlwpk.cfg
+
+backup() {
+  ns=$1
+  kubectl get po -n $ns| grep jummp | xargs -n 1|grep jummp > tmp/pos.txt
+ 
+  file=tmp/pos.txt
+  pods_str=$(cat $file | tr "\n" " ")
+  pods=($pods_str)
+  for po in "${pods}"
+  do
+    dir="logs/$ns-$pos"
+    mkdir $dir
+    kubectl logs $po -n $ns > "$dir/$pos.log" 
+    kubectl cp $ns/$pos:/usr/local/tomcat/logs/ $dir/logs/
+  done
+
+}
+
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+if [[ $BRANCH == "k8sdev" ]]; then
+  backup bmdev
+else
+  backup bmprod
+fi
+
+
