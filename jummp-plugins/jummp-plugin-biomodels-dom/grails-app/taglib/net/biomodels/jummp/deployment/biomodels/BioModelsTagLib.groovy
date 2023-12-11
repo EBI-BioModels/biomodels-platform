caee3b0ef094501716c2936c573faeaa5f0efda7
Tung Nguyen <nvntung@gmail.com>
2023-12-11 12:42:07 +0000

    JBM-619 better to use SBGN logo

--- jummp-plugins/jummp-plugin-biomodels-dom/grails-app/taglib/net/biomodels/jummp/deployment/biomodels/BioModelsTagLib.groovy
+++ jummp-plugins/jummp-plugin-biomodels-dom/grails-app/taglib/net/biomodels/jummp/deployment/biomodels/BioModelsTagLib.groovy
@@ -374,8 +374,12 @@ class BioModelsTagLib {
     }
 
     def renderLinkToNewtEditor = { Map attrs ->
+        out << '<div id="newt-editor-link-placeholder" style="padding-top:1em">'
         out << render(template: "/templates/biomodels/modelDisplay/linkNewtEditor",
             model: [hrefLinkToNewtEditor: attrs.hrefLinkToNewtEditor, serverURL: attrs.serverURL])
+        out << '</div>'
+    }
+
     def doRenderOrAddGalaxyLink = { Map attrs ->
         String modelId = attrs?.modelId as String
         String href = "https://usegalaxy.eu/root?tool_id=biomodels_${modelId?.toLowerCase()}"
