180f07400173157fc8a8dffabce38d4a275acf17
Tung Nguyen <nvntung@gmail.com>
2024-04-04 08:42:04 +0100

    JBM-649 revise ContributionDetailsWithoutInvite domain class

--- grails-app/migrations/20240328_createContributionDetailsWithoutInviteTable.groovy
+++ grails-app/migrations/20240328_createContributionDetailsWithoutInviteTable.groovy
@@ -18,9 +18,7 @@ databaseChangeLog = {
 				constraints(nullable: "false")
 			}
 
-			column(name: "orcid", type: "varchar(255)") {
-				constraints(nullable: "false")
-			}
+			column(name: "orcid", type: "varchar(255)")
 
 			column(name: "revision_id", type: "bigint") {
 				constraints(nullable: "false")
@@ -33,14 +31,42 @@ databaseChangeLog = {
 	}
 
 	changeSet(author: "tnguyen (generated)", id: "1711660886873-2") {
-		createIndex(indexName: "FK_6sor51ubrsxg6vdt3c3xae0hq", tableName: "contribution_details_without_invite") {
+		createIndex(indexName: "FK_6sor51ubrsxg6vdt3c3xae0hq",
+            tableName: "contribution_details_without_invite") {
 			column(name: "revision_id")
 		}
 	}
 
 	changeSet(author: "tnguyen (generated)", id: "1711660886873-3") {
-		createIndex(indexName: "FK_7icuj7do89ftlb6dgx7tmffnv", tableName: "contribution_details_without_invite") {
+		createIndex(indexName: "FK_7icuj7do89ftlb6dgx7tmffnv",
+            tableName: "contribution_details_without_invite") {
 			column(name: "role_id")
 		}
 	}
+
+    changeSet(author: "tnguyen (generated)", id: "1712215210313-1") {
+        createIndex(indexName: "orcid_uniq_1712215209477",
+            tableName: "contribution_details_without_invite", unique: "true") {
+            column(name: "orcid")
+        }
+    }
+
+    changeSet(author: "tnguyen (generated)", id: "1712215210313-7") {
+        addForeignKeyConstraint(baseColumnNames: "revision_id",
+            baseTableName: "contribution_details_without_invite",
+            constraintName: "FK_6sor51ubrsxg6vdt3c3xae0hq",
+            deferrable: "false", initiallyDeferred: "false",
+            referencedColumnNames: "id", referencedTableName: "revision",
+            referencesUniqueColumn: "false")
+    }
+
+    changeSet(author: "tnguyen (generated)", id: "1712215210313-8") {
+        addForeignKeyConstraint(baseColumnNames: "role_id",
+            baseTableName: "contribution_details_without_invite",
+            constraintName: "FK_7icuj7do89ftlb6dgx7tmffnv",
+            deferrable: "false", initiallyDeferred: "false",
+            referencedColumnNames: "id",
+            referencedTableName: "contribution_role",
+            referencesUniqueColumn: "false")
+    }
 }
