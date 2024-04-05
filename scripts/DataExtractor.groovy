07682b8c1768152ce38794cdc493ae9750acfe7f
Tung Nguyen <nvntung@gmail.com>
2024-04-05 17:35:20 +0100

    JBM-471 continue refactoring scripts to improve reusability

--- scripts/DataExtractor.groovy
+++ scripts/DataExtractor.groovy
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2010-2018 EMBL-European Bioinformatics Institute (EMBL-EBI),
+ * Copyright (C) 2010-2024 EMBL-European Bioinformatics Institute (EMBL-EBI),
  * Deutsches Krebsforschungszentrum (DKFZ)
  *
  * This file is part of Jummp.
@@ -18,87 +18,22 @@
  * with Jummp; if not, see <http://www.gnu.org/licenses/agpl-3.0.html>.
  */
 
-
-
-import grails.plugin.springsecurity.SpringSecurityUtils
 import groovyx.gpars.GParsPool
 import net.biomodels.jummp.core.model.ModelState
 import net.biomodels.jummp.model.Revision
-import net.biomodels.jummp.plugins.security.User
-import net.biomodels.jummp.plugins.security.UserRole
+import net.biomodels.jummp.utils.RunScriptHelper
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
-import org.springframework.security.core.Authentication
-import org.springframework.security.core.GrantedAuthority
-import org.springframework.security.core.context.SecurityContextHolder
 
 import java.time.Duration
 import java.time.Instant
+
 /**
  * @author Tung Nguyen <nvntung@gmail.com> on 20/09/2023
  */
-class DataExtractor {
+class DataExtractor extends RunScriptHelper {
     def ctx
-
     private static final Logger logger = LoggerFactory.getLogger(DataExtractor.class)
-
-    static final String adminUsername = System.getenv("ADMIN_USER")
-    // auth token for admin account; used by worker threads to publish models
-    static final Authentication adminAuth = createTokenForUser(adminUsername)
-
-    /*
-     * Creates and returns an authentication token for the given user.
-     * Note that the owner of the credentials is *not* authenticated until the token is put in the SecurityContext
-     * (see simpleRunAs()). Also note that, by default, SecurityContexts are thread-local.
-     *
-     * @param username the username for the account whose credentials should be used to create the authentication token
-     * @return the authentication token which, if placed in the security context of the current thread, would allow us
-     *      to log in as the given user.
-     */
-    private static UsernamePasswordAuthenticationToken createTokenForUser(User u) {
-        assert u
-        def authorities = UserRole.findAllByUser(u)*.role*.authority.join(",")
-        List<GrantedAuthority> roles = SpringSecurityUtils.parseAuthoritiesString authorities
-        return new UsernamePasswordAuthenticationToken(u.username, u.password, roles)
-    }
-
-    /*
-     * Creates and returns an authentication token for the given user.
-     *
-     * @param username the username for the account whose credentials should be used to create the authentication token
-     * @return an authentication token for the given user
-     * @see {@link Ctx#createTokenForUser(net.biomodels.jummp.plugins.security.User)}
-     */
-    private static UsernamePasswordAuthenticationToken createTokenForUser(String username) {
-        assert username : "Username required but not defined"
-        createTokenForUser(User.findByUsername(username))
-    }
-
-    /**
-     * Very simple means of executing an action as a different user than the one
-     * that is currently authenticated.
-     *
-     * Performs the action defined by closure as the user defined by Authentication
-     * object auth, then reverts to the original authentication.
-     *
-     * Suitable for lightweight work for which we need not create a separate thread.
-     */
-    static def simpleRunAs = { auth, closure ->
-        def currentAuth
-        try {
-            currentAuth = SecurityContextHolder.context.authentication
-            SecurityContextHolder.context.authentication = auth
-            def result = closure.call()
-            return result
-        } finally {
-            if (currentAuth) {
-                SecurityContextHolder.context.authentication = currentAuth
-            } else {
-                SecurityContextHolder.clearContext()
-            }
-        }
-    }
 
     void lookForSedMlFileInModel(final String submissionId) {
         List files = ctx.modelDelegateService.retrieveModelFiles(submissionId)
@@ -114,7 +49,8 @@ class DataExtractor {
     /**
      * Gets all SBML based models which one of the additional files is in SEDML format
      */
-    void findSbmlBasedModelsHavingSedML() {
+    @Override
+    void run() {
         String duration
         Instant startTime = Instant.now()
         ctx.persistenceInterceptor?.init()
@@ -163,4 +99,4 @@ Cannot retrieve the files of model ${submissionId} because of the below error:\n
     }
 }
 
-new DataExtractor(ctx: ctx).findSbmlBasedModelsHavingSedML()
+new DataExtractor(ctx: ctx).run()
--- src/groovy/net/biomodels/jummp/utils/RunScriptHelper.groovy
+++ src/groovy/net/biomodels/jummp/utils/RunScriptHelper.groovy
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2010-2020 EMBL-European Bioinformatics Institute (EMBL-EBI),
+ * Copyright (C) 2010-2024 EMBL-European Bioinformatics Institute (EMBL-EBI),
  * Deutsches Krebsforschungszentrum (DKFZ)
  *
  * This file is part of Jummp.
@@ -24,6 +24,7 @@ import grails.plugin.springsecurity.SpringSecurityUtils
 import net.biomodels.jummp.plugins.security.User
 import net.biomodels.jummp.plugins.security.UserRole
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.Authentication
 import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.context.SecurityContextHolder
 
@@ -32,7 +33,14 @@ import org.springframework.security.core.context.SecurityContextHolder
  * @author <a href="mailto:tung.nguyen@ebi.ac.uk">Tung Nguyen</a>
  * @author <a href="mailto:mihai.glont@ebi.ac.uk">Mihai Glont</a>
  */
-class RunScriptHelper {
+abstract class RunScriptHelper {
+    def ctx
+
+    static final String ADMIN_USERNAME = System.getenv("ADMIN_USERNAME")
+
+    // auth token for admin account; used by worker threads to publish models
+    static final Authentication adminAuth = createTokenForUser(ADMIN_USERNAME)
+
     /**
      * Creates and returns an authentication token for the given user.
      * Note that the owner of the credentials is *not* authenticated until the token is put in the SecurityContext
@@ -73,11 +81,11 @@ class RunScriptHelper {
     static def simpleRunAs = { auth, closure ->
         def currentAuth
         try {
-            currentAuth = SecurityContextHolder.context.authentication
             SecurityContextHolder.context.authentication = auth
             def result = closure.call()
             return result
         } finally {
+            currentAuth = SecurityContextHolder.context.authentication
             if (currentAuth) {
                 SecurityContextHolder.context.authentication = currentAuth
             } else {
@@ -85,4 +93,6 @@ class RunScriptHelper {
             }
         }
     }
+
+    abstract void run()
 }
