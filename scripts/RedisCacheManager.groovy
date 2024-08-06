b25aea501d633222893a827e789535b5a44ada30
Tung Nguyen <nvntung@gmail.com>
2024-08-06 16:49:02 +0100

    JBM-647 log, get rid of unused and stop println statements

--- scripts/RedisCacheManager.groovy
+++ scripts/RedisCacheManager.groovy
@@ -2,8 +2,11 @@ import net.biomodels.jummp.core.annotation.ElementAnnotationTransportCommand as 
 import net.biomodels.jummp.core.adapters.RevisionAdapter
 import net.biomodels.jummp.model.Model
 import net.biomodels.jummp.model.Revision
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 class RedisCacheManager {
+    private final Logger LOGGER = LoggerFactory.getLogger(RedisCacheManager.class)
     def ctx
 
     void run() {
@@ -14,7 +17,7 @@ class RedisCacheManager {
     void retrieveAnnotations() {
         def mDS = ctx.getBean("modelDelegateService")
         List<String> listAllIdentifiers = mDS.getAllModelIdentifiers()
-        List tenFirstIds = listAllIdentifiers.take(10)
+        List tenFirstIds = listAllIdentifiers.take(5)
         //String modelId = "MODEL8389825246"
         tenFirstIds.each { String modelId ->
             doRetrieveAnnotations modelId
@@ -22,7 +25,6 @@ class RedisCacheManager {
     }
 
     def doRetrieveAnnotations(String modelId) {
-        def mS = ctx.getBean("modelService")
         def mdDS = ctx.getBean("metadataDelegateService")
         def redis = ctx.getBean("redisService")
 
@@ -32,31 +34,35 @@ class RedisCacheManager {
         } else {
             model = Model.findByPublicationId(modelId)
         }
+        if (!model) {
+            LOGGER.info("$modelId doesn't exist.")
+            return
+        }
         Revision[] pairFirstLastRev = getFirstAndLastRevision(model)
         Revision latest = pairFirstLastRev[1]
         def revTC = new RevisionAdapter(revision: latest, latest: true).toCommandObject()
-        long revisionId = latest?.id
         List<EATC> annotations = mdDS.fetchAnnotations(revTC)
         List statements = annotations*.statement
         statements = statements.unique { it.object.uri }
         String hasTaxon = ""
         String strOfAnnotations = ""
         statements.each {
-            println "${model.submissionId}\t${it.predicate.accession}\t${it.object.datatype}\t${it.object.uri}"
+            //println "${model.submissionId}\t${it.predicate.accession}\t${it.object.datatype}\t${it.object.uri}"
             if (it.predicate.accession == "hasTaxon" && it.object.datatype == "taxonomy") {
                 hasTaxon = "${it.object.accession}|${it.object.name}|${it.object.uri}"
             }
             strOfAnnotations += "${it.predicate.accession}\t${it.object.datatype}\t${it.object.uri}\t${it.object.name}|"
         }
 
-        strOfAnnotations = strOfAnnotations.substring(0, strOfAnnotations.length() - 1) // remove the last pile - vertical line
+        // remove the last pile - vertical line
+        strOfAnnotations = strOfAnnotations.substring(0, strOfAnnotations.length() - 1)
         redis.doRedisHSetNX(model.submissionId, "annotations", strOfAnnotations)
 
         if (hasTaxon) {
             redis.doRedisHSetNX(model.submissionId, "organism", hasTaxon)
         }
 
-        println statements.size()
+        //println statements.size()
     }
 
     protected Revision[] getFirstAndLastRevision(Model model) {
