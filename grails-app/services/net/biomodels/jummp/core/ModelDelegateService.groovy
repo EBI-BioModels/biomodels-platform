9babbbabadc40211f8b2a4f4bb52c0ef9bf6fb35
Tung Nguyen <nvntung@gmail.com>
2024-11-05 15:42:17 +0000

    JBM-617 fixup the previous commit

--- grails-app/services/net/biomodels/jummp/core/ModelDelegateService.groovy
+++ grails-app/services/net/biomodels/jummp/core/ModelDelegateService.groovy
@@ -329,8 +329,8 @@ session: ${TransactionSynchronizationManager.getResource(Holders.applicationCont
                 privateRevs.add(revision.revisionNumber)
             }
         }
-        publishedRevs = Collections.sort(publishedRevs, Collections.reverseOrder())
-        privateRevs = Collections.sort(privateRevs, Collections.reverseOrder())
+        Collections.sort(publishedRevs, Collections.reverseOrder())
+        Collections.sort(privateRevs, Collections.reverseOrder())
         ["vcsId"      : vcsId, "publishedRevs": publishedRevs,
          "privateRevs": privateRevs, "submissionId": model.submissionId]
     }
