15d3405b4c168215b37b31ceb893a9072964d530
Tung Nguyen <nvntung@gmail.com>
2025-01-01 05:59:39 +0000

    JBM-604 resolved NPE

--- jummp-plugins/jummp-plugin-core-api/src/groovy/net/biomodels/jummp/utils/WebServiceFetcher.groovy
+++ jummp-plugins/jummp-plugin-core-api/src/groovy/net/biomodels/jummp/utils/WebServiceFetcher.groovy
@@ -98,13 +98,16 @@ caused by ${conn.responseCode}: ${conn.getErrorStream().inspect()}""")
 
     static String executePostRequest(final String serviceURI, final String requestBody) {
         CloseableHttpClient httpClient = HttpClientBuilder.create().build()
-        InetSocketAddress addr = (InetSocketAddress) proxy.address()
-        if (addr != null) {
-            System.out.println("proxy hostname : " + addr.getHostName());
-            System.out.println("proxy port : " + addr.getPort());
-            HttpHost host = new HttpHost(addr.getHostName(), addr.getPort())
-            httpClient = HttpClientBuilder.create().setProxy(host).build()
-        }
+//        InetSocketAddress addr = (InetSocketAddress) proxy.address()
+//        if (addr != null) {
+//            System.out.println("proxy hostname : " + addr.getHostName());
+//            System.out.println("proxy port : " + addr.getPort());
+//            HttpHost host = new HttpHost(addr.getHostName(), addr.getPort())
+        def hostName = System.getenv("HTTP_PROXY_HOST")
+        def hostPort = System.getenv("HTTP_PROXY_PORT") as int
+        HttpHost host = new HttpHost(hostName, hostPort)
+        httpClient = HttpClientBuilder.create().setProxy(host).build()
+//        }
         String result = null
         try {
             HttpPost request = new HttpPost(serviceURI)
