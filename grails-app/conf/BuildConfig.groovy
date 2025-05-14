f0198c9aa65c836e2bd0cb6114fb2adb14c02478
Tung Nguyen <nvntung@gmail.com>
2025-05-14 16:42:21 +0100

    JBM-682 sync the latest version

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -88,7 +88,7 @@ grails.project.dependency.resolution = {
         runtime 'mysql:mysql-connector-java:5.1.34'
         runtime "postgresql:postgresql:9.1-901.jdbc4"
 
-        compile "uk.ac.ebi.ddi:ddi-ebe-ws-dao:1.2.1"
+        compile "uk.ac.ebi.ddi:ddi-ebe-ws-dao:1.3-SNAPSHOT"
         // Jackson DataBinder has 'provided' scope in DDI: See
         //      https://github.com/BD2K-DDI/ddi-base-master/blob/2326b4/pom.xml
         //      https://github.com/BD2K-DDI/ddi-ebeye-ws-dao/blob/8bd08f/pom.xml
