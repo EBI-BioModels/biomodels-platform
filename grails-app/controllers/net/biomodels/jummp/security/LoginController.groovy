bafce99a17b8008f33bc6322ffb5a4cfbdf71246
Tung Nguyen <nvntung@gmail.com>
2025-06-06 21:40:29 +0100

    JBM-595 get the previous request's link back

--- grails-app/controllers/net/biomodels/jummp/security/LoginController.groovy
+++ grails-app/controllers/net/biomodels/jummp/security/LoginController.groovy
@@ -89,7 +89,7 @@ class LoginController extends CommonController {
         String previousURL = request.getHeader("referer")
         String j_previousURL = request.getHeader("referer")
         String view = 'auth'
-        String postUrl = "${serverURL}${config.apf.filterProcessesUrl}"
+        String postUrl = "${request.contextPath}${config.apf.filterProcessesUrl}"
         render view: view, model: [postUrl: postUrl, previousURL: previousURL, j_previousURL: j_previousURL,
                                    rememberMeParameter: config.rememberMe.parameter,
                                    flashMessage: flash.flashMessage?:""]
